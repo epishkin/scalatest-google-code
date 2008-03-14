e705960b4a3d8fd37bad45f2b1d1020433ccd718
bv <bv>
2008-03-14 21:50:24 +0000

    I think this all works except for the code generation.

--- src/org/scalatest/fun/FunSuite1.scala
+++ src/org/scalatest/fun/FunSuite1.scala
@@ -19,13 +19,6 @@ import scala.collection.immutable.ListSet
 import java.util.ConcurrentModificationException
 import java.util.concurrent.atomic.AtomicReference
 
-/*
-I figured out that TreeSet doesn't override equals, so it may
-be that I thought I was testing the order of test names in my
-Suite tests, and it was only looking at what names were in the Set.
-Here, I'd like the order to be the order in which the "test" calls 
-appear in the file.
-*/
 trait FunSuite1[F] extends Suite {
 
   // Until it shows up in Predef
