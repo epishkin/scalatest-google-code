c6a65aed624ae79f336f19a672a3182c4f508d51
bv <bv>
2008-05-07 20:37:54 +0000

    Added scaladoc text on ignore to FunSuite.

--- app/src/org/scalatest/fun/FunSuite.scala
+++ app/src/org/scalatest/fun/FunSuite.scala
@@ -48,7 +48,7 @@ import java.util.concurrent.atomic.AtomicReference
 abstract class Group(val name: String)
 
 /**
- * A suite of tests in which each test is represented as a function value. The "<code>Fun</code>" in <code>FunSuite</code> stands for functional.
+ * A suite of tests in which each test is represented as a function value. The &#8220;<code>Fun</code> &#8221;in <code>FunSuite</code> stands for functional.
  * Here's an example <code>FunSuite</code>:
  *
  * <pre>
@@ -70,12 +70,12 @@ abstract class Group(val name: String)
  * }
  * </pre>
  *
- * "<code>test</code>" is a method defined in <code>FunSuite</code>, which will be invoked
+ * &#8220;<code>test</code>&#8221; is a method defined in <code>FunSuite</code>, which will be invoked
  * by the primary constructor of <code>MySuite</code>. You specify the name of the test as
  * a string between the parentheses, and the test code itself between curly braces.
  * The test code is a function passed as a by-name parameter to <code>test</code>, which registers
  * it for later execution. One benefit of <code>FunSuite</code> compared to <code>Suite</code> is you need not name all your
- * tests starting with "<code>test</code>". In addition, you can more easily give long names to
+ * tests starting with &#8220;<code>test</code>.&#8221; In addition, you can more easily give long names to
  * your tests, because you need not encode them in camel case, as you must do
  * with test methods.
  *
@@ -151,7 +151,6 @@ abstract class Group(val name: String)
  * object SlowTest extends Group("com.mycompany.groups.SlowTest")
  * object DBTest extends Group("com.mycompany.groups.DBTest")
  * </pre>
- *
  * <p>
  * Given these definitions, you could place <code>FunSuite</code> tests into groups like this:
  * </p>
@@ -190,6 +189,53 @@ abstract class Group(val name: String)
  *
  * <p>
  * <strong>Ignored tests</strong>
+ * </p>
+ *
+ * <p>
+ * To support the common use case of &#8220;temporarily&#8221; disabling tests, with the
+ * good intention of resurrecting the test at a later time, <code>FunSuite</code> provides registration
+ * methods that start with <code>ignore</code> instead of <code>test</code>. For example, to temporarily
+ * disable the test named <code>addition</code>, just change &#8220;<code>test</code>&#8221; into &#8220;<code>ignore</code>,&#8221; like this:
+ * </p>
+ *
+ * <pre>
+ * import org.scalatest.FunSuite
+ *
+ * class MySuite extends FunSuite {
+ *
+ *   ignore("addition") {
+ *     val sum = 1 + 1
+ *     assert(sum === 2)
+ *     assert(sum + 2 === 4)
+ *   }
+ *
+ *   test("subtraction") {
+ *     val diff = 4 - 1
+ *     assert(diff === 3)
+ *     assert(diff - 2 === 1)
+ *   }
+ * }
+ * </pre>
+ *
+ * <p>
+ * If you run this version of <code>MySuite</code> with:
+ * </p>
+ *
+ * <pre>
+ * scala> (new MySuite).execute()
+ * </pre>
+ *
+ * <p>
+ * It will run only <code>subtraction</code> and report that <code>addition</code> was ignored.
+ * </p>
+ *
+ * <p>
+ * As with <code>org.scalatest.Suite</code>, the ignore feature is implemented as a group. The <code>execute</code> method that takes no parameters
+ * adds <code>org.scalatest.Ignore</code> to the <code>excludes</code> <code>Set</code> it passes to
+ * the primary <code>execute</code> method, as does <code>Runner</code>. The only difference between
+ * <code>org.scalatest.Ignore</code> and the groups you may define and exclude is that ScalaTest reports
+ * ignored tests to the <code>Reporter</code>. The reason ScalaTest reports ignored tests is as a feeble
+ * attempt to encourage ignored tests to be eventually fixed and added back into the active suite of tests.
  * </p>
  *
  * <p>
