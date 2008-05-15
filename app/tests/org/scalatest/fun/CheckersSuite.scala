1b9b9a5b8f32debbe500d69aabe49c68009ecf28
bv <bv>
2008-05-15 20:44:27 +0000

    This seems to hang the compiler. Checking in before going back.

--- app/tests/org/scalatest/fun/CheckersSuite.scala
+++ app/tests/org/scalatest/fun/CheckersSuite.scala
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.scalatest.props
+package org.scalatest.fun
 
 import org.scalacheck._
 import Arbitrary._
