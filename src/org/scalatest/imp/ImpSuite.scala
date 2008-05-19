7b40eaeabdd5baf0527527807d68b900e8da91ee
bv <bv>
2008-05-19 22:17:58 +0000

    Moving ImpSuite to the imp package.

--- src/org/scalatest/imp/ImpSuite.scala
+++ src/org/scalatest/imp/ImpSuite.scala
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.scalatest
+package org.scalatest.imp
 
 /**
  * Trait that can be mixed into suites that need methods invoked before and after executing the
