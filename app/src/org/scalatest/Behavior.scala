79592ed83c069ee7a27d97971ef6db10c306e689
bv <bv>
2008-05-25 14:38:03 +0000

    Went back to Behavior requiring no behavior name passed to its constructor. Will try an execute that takes a prefix instead.

--- app/src/org/scalatest/Behavior.scala
+++ app/src/org/scalatest/Behavior.scala
@@ -1,3 +1,8 @@
 package org.scalatest
 
-abstract class Behavior(val behaviorName: String) extends Spec
+abstract class Behavior extends Spec {
+  def execute(testName: Option[String], reporter: Reporter, stopper: Stopper, includes: Set[String], excludes: Set[String],
+              properties: Map[String, Any], distributor: Option[Distributor], prefix: String) {
+    super.execute(testName, reporter, stopper, includes, excludes, properties, distributor)
+  }
+}
