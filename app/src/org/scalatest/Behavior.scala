51b82b72366ef618e987a27d75ff31eac44eb628
bv <bv>
2008-06-02 01:41:54 +0000

    Dropping trait not needed for 0.9.3, so it won't show up in src.

--- app/src/org/scalatest/Behavior.scala
+++
@@ -1,8 +0,0 @@
-package org.scalatest
-
-private[scalatest] abstract class Behavior extends Spec {
-  def execute(testName: Option[String], reporter: Reporter, stopper: Stopper, includes: Set[String], excludes: Set[String],
-              properties: Map[String, Any], distributor: Option[Distributor], prefix: String) {
-    super.execute(testName, reporter, stopper, includes, excludes, properties, distributor)
-  }
-}
