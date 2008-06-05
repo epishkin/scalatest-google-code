a796d33cbea235af6f11bafdd12ab0a9a941bc66
bv <bv>
2008-06-05 00:47:30 +0000

    Got rid of an odd import.

--- app/src/org/scalatest/CatchReporter.scala
+++ app/src/org/scalatest/CatchReporter.scala
@@ -15,7 +15,6 @@
  */
 package org.scalatest
 
-import org.scalatest.CatchReporter.handleReporterException
 import java.io.PrintStream
 
 /**
@@ -50,7 +49,7 @@ private[scalatest] class CatchReporter(reporter: Reporter, out: PrintStream) ext
       methodCall(reporter)
     }
     catch {
-      case e: Exception => handleReporterException(e, methodName, out)
+      case e: Exception => CatchReporter.handleReporterException(e, methodName, out)
     }
   }
 }
