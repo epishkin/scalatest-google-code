76dbd0f4372415da616ffcc4371ccf11176ee276
bv <bv>
2008-06-05 01:26:25 +0000

    Got the tests to work again after rearranging Behavior & Spec. Behavior is now the supertrait, Spec the subtrait that mixes in Suite.

--- app/src/org/scalatest/NodeFamily.scala
+++ app/src/org/scalatest/NodeFamily.scala
@@ -10,6 +10,6 @@ private[scalatest] object NodeFamily {
   }
   case class Trunk extends Branch(None, -1)
   case class Example(parent: Branch, exampleFullName: String, exampleShortName: String, override val level: Int, f: () => Unit) extends Node(Some(parent), level)
-  case class SharedBehaviorNode(parent: Branch, sharedBehavior: Spec, override val level: Int) extends Node(Some(parent), level)
+  case class SharedBehaviorNode(parent: Branch, sharedBehavior: Behavior, override val level: Int) extends Node(Some(parent), level)
   case class Description(parent: Branch, descriptionName: String, override val level: Int) extends Branch(Some(parent), level)
 }
