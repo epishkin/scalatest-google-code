cd06c4a1b97bdfbcbbb154e762cd9af20805ad69
bv <bv>
2008-06-16 19:37:25 +0000

    Severed the inheritance relationship between Spec and Behavior. Will now reduce code duplication.

--- src/org/scalatest/Spec.scala
+++ src/org/scalatest/Spec.scala
@@ -34,8 +34,56 @@ import NodeFamily._
  * }
  * @author Bill Venners
  */
-trait Spec extends Suite with Behavior {
+trait Spec extends Suite {
 
+  private[scalatest] val trunk: Trunk = new Trunk
+  private var currentBranch: Branch = trunk
+
+  class ItWord {
+    def should(exampleName: String) = new Inifier(exampleName)
+    def should(behaveWord: BehaveWord) = new Likifier()
+  }
+  
+  class BehaveWord {}
+
+  private def registerExample(exampleRawName: String, needsShould: Boolean, f: => Unit) {
+    currentBranch.subNodes ::=
+      Example(currentBranch, getExampleFullName(exampleRawName, needsShould, currentBranch), exampleRawName, needsShould, getExampleShortName(exampleRawName, needsShould, currentBranch), currentBranch.level + 1, f _)
+  }
+  
+  def specify(exampleRawName: String)(f: => Unit) {
+    registerExample(exampleRawName, false, f)
+  }
+    
+  class Inifier(exampleRawName: String) {
+    def in(f: => Unit) {
+      registerExample(exampleRawName, true, f)
+    }
+  }
+
+  protected val behave = new BehaveWord
+  class Likifier {
+    def like(sharedBehavior: Behavior) {
+      currentBranch.subNodes :::= sharedBehavior.trunk.subNodes.map(transformSharedExamplesFullName(_, currentBranch))
+      println("%$%$%$%$%$%$% sharedBehavior.trunk.subNodes: " + sharedBehavior.trunk.subNodes)
+    }
+  }
+  
+  protected val it = new ItWord
+
+  protected def describe(name: String)(f: => Unit) {
+    
+    def insertBranch(newBranch: Branch, f: () => Unit) {
+      val oldBranch = currentBranch
+      currentBranch.subNodes ::= newBranch
+      currentBranch = newBranch
+      f()
+      currentBranch = oldBranch
+    }
+
+    insertBranch(Description(currentBranch, name, currentBranch.level + 1), f _)
+  }
+  
   private def runTestsInBranch(branch: Branch, reporter: Reporter, stopper: Stopper) {
     branch match {
       case desc @ Description(_, descriptionName, level) => {
