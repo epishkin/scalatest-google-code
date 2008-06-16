1613b1c9d469112c07dff6cc127b6f60259cc0f2
bv <bv>
2008-06-16 18:36:23 +0000

    Moved helper functions to NodeFamily singleton.

--- app/src/org/scalatest/Behavior.scala
+++ app/src/org/scalatest/Behavior.scala
@@ -7,23 +7,9 @@ trait Behavior {
   private[scalatest] val trunk: Trunk = new Trunk
   private var currentBranch: Branch = trunk
 
-  private[scalatest] def countTestsInBranch(branch: Branch): Int = {
-    var count = 0
-    branch.subNodes.reverse.foreach(
-      _ match {
-        case Example(parent, exampleFullName, exampleRawName, needsShould, exampleShortName, level, f) => count += 1
-        case SharedBehaviorNode(parent, sharedBehavior, level) => { 
-          count += countTestsInBranch(sharedBehavior.trunk) // TODO: Will need to handle includes and excludes?
-        }
-        case branch: Branch => count += countTestsInBranch(branch)
-      }
-    )
-    count
-  }
-
   private def registerExample(exampleRawName: String, needsShould: Boolean, f: => Unit) {
     currentBranch.subNodes ::=
-      Example(currentBranch, Behavior.getExampleFullName(exampleRawName, needsShould, currentBranch), exampleRawName, needsShould, Behavior.getExampleShortName(exampleRawName, needsShould, currentBranch), currentBranch.level + 1, f _)
+      Example(currentBranch, NodeFamily.getExampleFullName(exampleRawName, needsShould, currentBranch), exampleRawName, needsShould, NodeFamily.getExampleShortName(exampleRawName, needsShould, currentBranch), currentBranch.level + 1, f _)
   }
   
   def specify(exampleRawName: String)(f: => Unit) {
@@ -44,7 +30,7 @@ trait Behavior {
   private def transformTheParent(node: Node, newParent: Branch) = {
     node match {
       case Example(oldParent, exampleFullName, exampleRawName, needsShould, exampleShortName, level, f) => 
-        Example(newParent, Behavior.getExampleFullName(exampleRawName, needsShould, newParent), exampleRawName, needsShould, Behavior.getExampleShortName(exampleRawName, needsShould, newParent), level, f)
+        Example(newParent, NodeFamily.getExampleFullName(exampleRawName, needsShould, newParent), exampleRawName, needsShould, NodeFamily.getExampleShortName(exampleRawName, needsShould, newParent), level, f)
       case oldDesc @ Description(oldParent, descriptionName, level) =>
         val newDesc = Description(newParent, descriptionName, level)
         newDesc.subNodes = oldDesc.subNodes
@@ -57,7 +43,7 @@ trait Behavior {
   
     node match {
       case Example(oldParent, exampleFullName, exampleRawName, needsShould, exampleShortName, level, f) => 
-        Example(oldParent, Behavior.getExampleFullName(exampleRawName, needsShould, currentBranch), exampleRawName, needsShould, Behavior.getExampleShortName(exampleRawName, needsShould, currentBranch), level, f)
+        Example(oldParent, NodeFamily.getExampleFullName(exampleRawName, needsShould, currentBranch), exampleRawName, needsShould, NodeFamily.getExampleShortName(exampleRawName, needsShould, currentBranch), level, f)
       case oldDesc @ Description(oldParent, descriptionName, level) =>
         val newDesc = Description(newParent, descriptionName, level)
         newDesc.subNodes = oldDesc.subNodes.map(transformTheParent(_, newDesc))
@@ -92,92 +78,3 @@ trait Behavior {
   }
 }
 
-private[scalatest] object Behavior {
-  protected[scalatest] def getPrefix(branch: Branch): String = {
-    branch match {
-       case Trunk() => ""
-      // Call to getPrefix is not tail recursive, but I don't expect the describe nesting to be very deep
-      case Description(parent, descriptionName, level) => Resources("prefixSuffix", getPrefix(parent), descriptionName)
-    }
-  }
-  
-    private[scalatest] def getExampleFullName(exampleRawName: String, needsShould: Boolean, parent: Branch): String = {
-    val prefix = Behavior.getPrefix(parent).trim
-    if (prefix.isEmpty) {
-      if (needsShould) {
-        // class MySpec extends Spec {
-        //   it should "pop when asked" in {}
-        // }
-        // Should yield: "it should pop when asked"
-        Resources("itShould", exampleRawName)
-      }
-      else {
-        // class MySpec extends Spec {
-        //   specify("It sure ought to pop when asked") {}
-        // }
-        // Should yield: "It sure ought to pop when asked"
-        exampleRawName
-      }
-    }
-    else {
-      if (needsShould) {
-        // class MySpec extends Spec {
-        //   describe("A Stack") {
-        //     it should "pop when asked" in {}
-        //   }
-        // }
-        // Should yield: "A Stack should pop when asked"
-        Resources("prefixShouldSuffix", prefix, exampleRawName)
-      }
-      else {
-        // class MySpec extends Spec {
-        //   describe("A Stack") {
-        //     specify("must pop when asked") {}
-        //   }
-        // }
-        // Should yield: "A Stack must pop when asked"
-        Resources("prefixSuffix", prefix, exampleRawName)
-      }
-    }
-  }
-
-  private[scalatest] def getExampleShortName(exampleRawName: String, needsShould: Boolean, parent: Branch): String = {
-    val prefix = Behavior.getPrefix(parent).trim
-    if (prefix.isEmpty) {
-      if (needsShould) {
-        // class MySpec extends Spec {
-        //   it should "pop when asked" in {}
-        // }
-        // Should yield: "it should pop when asked"
-        Resources("itShould", exampleRawName)
-      }
-      else {
-        // class MySpec extends Spec {
-        //   specify("It sure ought to pop when asked") {}
-        // }
-        // Should yield: "It sure ought to pop when asked"
-        exampleRawName
-      }
-    }
-    else {
-      if (needsShould) {
-        // class MySpec extends Spec {
-        //   describe("A Stack") {
-        //     it should "pop when asked" in {}
-        //   }
-        // }
-        // Should yield: "should pop when asked"
-        Resources("should", exampleRawName)
-      }
-      else {
-        // class MySpec extends Spec {
-        //   describe("A Stack") {
-        //     specify("must pop when asked") {}
-        //   }
-        // }
-        // Should yield: "A Stack must pop when asked"
-        exampleRawName
-      }
-    }
-  }
-}
