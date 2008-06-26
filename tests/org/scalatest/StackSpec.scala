ef36414ec209c5c2646e04a39e7631ce39dfe458
bv <bv>
2008-06-26 16:42:17 +0000

    Moved Stack outside of StackSpec.

--- tests/org/scalatest/StackSpec.scala
+++ tests/org/scalatest/StackSpec.scala
@@ -2,34 +2,35 @@ package org.scalatest
 
 import scala.collection.mutable.ListBuffer
 
+class Stack[T] {
+  val MAX = 10
+  private var buf = new ListBuffer[T]
+  def push(o: T) {
+    if (!full)
+      o +: buf
+    else
+      throw new IllegalStateException("can't push onto a full stack")
+  }
+  def pop(): T = {
+    if (!empty)
+      buf.remove(0)
+    else
+      throw new IllegalStateException("can't pop an empty stack")
+  }
+  def peek: T = {
+    if (!empty)
+      buf(0)
+    else
+      throw new IllegalStateException("can't pop an empty stack")
+  }
+  def full: Boolean = buf.size == MAX
+  def empty: Boolean = buf.size == 0
+  def size = buf.size
+}
+      
+
 class StackSpec extends Spec {
 
-  class Stack[T] {
-    val MAX = 10
-    private var buf = new ListBuffer[T]
-    def push(o: T) {
-      if (!full)
-        o +: buf
-      else
-        throw new IllegalStateException("can't push onto a full stack")
-    }
-    def pop(): T = {
-      if (!empty)
-        buf.remove(0)
-      else
-        throw new IllegalStateException("can't pop an empty stack")
-    }
-    def peek: T = {
-      if (!empty)
-        buf(0)
-      else
-        throw new IllegalStateException("can't pop an empty stack")
-    }
-    def full: Boolean = buf.size == MAX
-    def empty: Boolean = buf.size == 0
-    def size = buf.size
-  }
-      
   // Fixture creation methods
   def emptyStack = new Stack[Int]
   def fullStack = {
