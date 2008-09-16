3d02ebfe13d958b80288eb895b8d474f2ff3df05
bv <bv>
2008-09-16 16:49:39 +0000

    init ver

---
+++ tests/org/scalatest/matchers/MatcherSpec.scala
@@ -0,0 +1,62 @@
+package org.scalatest.matchers
+
+import Matchers._
+
+class MatcherSpec extends Spec {
+  "The equal matcher" -- {
+    "should do nothing when equal" - {
+      1 should equal (1)
+    }
+    "should throw an assertion error when not equal" - {
+      intercept(classOf[AssertionError]) {
+        1 should equal (2)
+      }
+    }
+  }
+  "The be matcher" -- {
+    "should do nothing when equal" - {
+      false should be (false)
+    }
+    "should throw an assertion error when not equal" - {
+      intercept(classOf[AssertionError]) {
+        false should be (true)
+      }
+    }
+  }
+  "The not matcher" -- {
+    "should do nothing when not true" - {
+      1 should not { be (2) }
+    }
+    "should throw an assertion error when true" - {
+      intercept(classOf[AssertionError]) {
+        1 should not { be (1) }
+      }
+    }
+  }
+  "The endsWith matcher" -- {
+    "should do nothing when true" - {
+      "Hello, world" should endWith ("world")
+    }
+    "should throw an assertion error when not true" - {
+      intercept(classOf[AssertionError]) {
+        "Hello, world" should endWith ("planet")
+      }
+    }
+  }
+
+  "The and matcher" -- {
+    "should do nothing when both operands are true" - {
+      1 should { equal (1) and equal (2 - 1) }
+    }
+    "should throw AssertionError when first operands is false" - {
+      intercept(classOf[AssertionError]) {
+        1 should (equal (2) and equal (1))
+      }
+    }
+    "should throw AssertionError when second operands is false" - {
+      intercept(classOf[AssertionError]) {
+        1 should (equal (1) and equal (2))
+      }
+    }
+  }
+}
