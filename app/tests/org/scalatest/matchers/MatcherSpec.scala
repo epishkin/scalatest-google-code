0da05996a345f63208f87039c6b37dc7f85b0c93
bv <bv>
2008-09-19 16:28:01 +0000

    putting messages in expectations into resources

--- app/tests/org/scalatest/matchers/MatcherSpec.scala
+++ app/tests/org/scalatest/matchers/MatcherSpec.scala
@@ -3,6 +3,8 @@ package org.scalatest.matchers
 import Matchers._
 
 class MatcherSpec extends Spec {
+
+
   "The equal matcher" -- {
     "should do nothing when equal" - {
       1 should equal (1)
@@ -180,11 +182,20 @@ class MatcherSpec extends Spec {
       // mySet should not { be (empty) }
     }
     /*
-     // After should, if an even number of tokens, you need parens on the last thing.
-     // If an odd number of tokens, you don't need parens on the last thing.
+     // After should/shouldNot, if an even number of tokens, you need parens on the last thing.
+     // If an odd number of tokens, you can't put parens on the last thing.
 
      map should have key 8
      map shouldNot have key 8
+
+     // I think I can get rid of the ugly not { ... } problem by not having a "not", and just have shouldNot, andNot, and orNot.
+     map should haveKey (8) // I need this version so they can be combined with and, or, not, etc.
+     map should { haveKey (8) and haveValue ("Fred") }
+     map should { haveKey (8) andNot haveValue ("Fred") }
+     // No, I think if have is a val of a special type that has a key method that takes a T (that matches the Map[T, U]), then
+     // the should method that takes that special type could just return the passed instance, and key would get invoked on that. Then
+     // I could say "have key 8" anywhere I think it would work, assuming I can get the types square.
+     map should { have key 8 andNot have value "Fred" }
 
      map should have value "eleven"
      map shouldNot have value "eleven"
@@ -197,18 +208,22 @@ class MatcherSpec extends Spec {
 
      iterable should beEmpty
      iterable shouldNot beEmpty
+     iterable should be ('empty)
+     iterable shouldNot be ('empty)
 
      string should have length 0
      string shouldNot have length 0
      string should when trimmed have length 0
      string shouldNot when trimmed have length 0
 
+     string should { not { have length 0 } and when trimmed have length 0 }
+
      array should have length 9
      array shouldNot have length 9
 
      // Using boolean properties dynamically
-     object should be ('empty)
-     object shouldNot be ('empty)
+     object should be ('hidden)
+     object shouldNot be ('hidden)
      object should be a 'file
      object shouldNot be a 'file
      object should be an 'openBook
@@ -218,53 +233,57 @@ class MatcherSpec extends Spec {
 
      list should be (Nil)
      list shouldNot be (Nil)
+     // list should beNil
+     // list shouldNot beNil
 
-     // of takes a type param but no value params, used in postfix notation
+     // anInstanceOf takes a type param but no value params, used in postfix notation
      object should be anInstanceOf[Something] 
      object shouldNot be anInstanceOf[Something] 
 
      object should be theSameInstanceAs anotherObjectReference
      object shouldNot be theSameInstanceAs anotherObjectReference
 
+     object should be (null)
+     object should beNull
+     object shouldNot beNull
+     object shouldNot be (null)
+
      string should equalIgnoringCase ("happy")
      string shouldNot equalIgnoringCase ("happy")
-     string should equalWhenTrimmed (" happy  ")
-     string shouldNot equalWhenTrimmed (" happy  ")
+     string should equalWhenTrimmed ("happy")
+     string shouldNot equalWhenTrimmed ("happy")
 
      string should startWith ("something")
      string shouldNot startWith ("something")
      string should endWith ("something")
      string shouldNot endWith ("something")
 
-     // Check if a string includes a substring
-     string should include ("bob")
-     string shouldNot include ("bob")
+     string should contain substring "bob"
+     string shouldNot contain substring "bob"
 
      string should matchRegEx ("""[a-zA-Z_]\w*""")
      string shouldNot matchRegEx ("""[a-zA-Z_]\w*""")
 
-     // Maybe these could be structural. Actually, I think these could
+     // I think these could
      // take a view bounds, something implicitly convertable to Ordered
      ordered should be > 7
      ordered should be >= 7
      ordered should be < 7
      ordered should be <= 7
 
-     // ordered should be greater than (7)
-     // ordered should be greater than or equal to 7
-     // ordered should be less than (7)
-     // ordered should be less than or equal to 7
+     floatingPointNumber should be (7.0 plusOrMinus 0.01)
+     floatingPointNumber should be (7.0 exactly)
 
-     floatingPointNumber should be ( 7.0 plusOrMinus 0.01 )
-     floatingPointNumber should be ( 7.0 exactly )
      option should beNone
+     option should be (None)
      option should beDefined
+     option should be ('defined)
      option should equal (Some(1))
-     option should be (some[String])
-     option should be (some[String], which should start with "prefix") // or this
+     option should be equalTo Some(1)
+     option should be some[String]
+     option should be (some[String], which should startWith ("prefix"))
      object should satisfy (_ > 12)
      object should satisfy { case 1 :: _ :: 3 :: Nil => true } 
-     // object should matchPattern { case 1 :: _ :: 3 :: Nil => true } // or..
 
      { "Howdy".charAt(-1) } shouldThrow (classOf[StringIndexOutOfBoundsException])
 
