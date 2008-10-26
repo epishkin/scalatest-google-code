d8630b0ca34f957150ff764323e666930798263c
bv <bv>
2008-10-26 14:23:32 +0000

    dropped runTests and runNestedSuites

--- app/src/org/scalatest/ExecuteAndRun.scala
+++ app/src/org/scalatest/ExecuteAndRun.scala
@@ -16,20 +16,4 @@ trait ExecuteAndRun {
     stopper: Stopper,
     goodies: Map[String, Any]
   )
-  protected def runTests(
-    testName: Option[String],
-    reporter: Reporter,
-    stopper: Stopper,
-    groupsToInclude: Set[String],
-    groupsToExclude: Set[String],
-    goodies: Map[String, Any]
-  )
-  protected def runNestedSuites(
-    reporter: Reporter,
-    stopper: Stopper,
-    groupsToInclude: Set[String],
-    groupsToExclude: Set[String],
-    goodies: Map[String, Any],
-    distributor: Option[Distributor]
-  )
 }
