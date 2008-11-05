097364b77c28dc3cea487230f4087bb3a529d2d7
bv <bv>
2008-11-05 18:59:03 +0000

    factored out some code dup in the StackSpecs into a trait for fixture creation methods

--- app/tests/org/scalatest/MustStackSpec.scala
+++ app/tests/org/scalatest/MustStackSpec.scala
@@ -44,29 +44,8 @@ trait MustStackBehaviors extends MustMatchers {
   }
 }
 
-class MustStackSpec extends MustSpec with MustStackBehaviors {
+class MustStackSpec extends MustSpec with StackFixtureCreationMethods with MustStackBehaviors {
 
-  // Fixture creation methods
-  def emptyStack = new Stack[Int]
-  def fullStack = {
-    val stack = new Stack[Int]
-    for (i <- 0 until stack.MAX)
-      stack.push(i)
-    stack
-  }
-  def stackWithOneItem = {
-    val stack = new Stack[Int]
-    stack.push(9)
-    stack
-  }
-  def stackWithOneItemLessThanCapacity = {
-    val stack = new Stack[Int]
-    for (i <- 1 to 9)
-      stack.push(i)
-    stack
-  }
-  val lastValuePushed = 9
-      
   "A Stack" -- {
 
     "(when empty)" -- {
