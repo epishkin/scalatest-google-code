ce568e1c05e39b5ec7427ffeff622236555c809a
bv <bv>
2008-12-19 22:59:21 +0000

    added IAEs thrown when duplicate names result in an Examples or an including of an Examples

---
+++ tests/org/scalatest/ExamplesSuite.scala
@@ -0,0 +1,38 @@
+package org.scalatest
+
+class ExamplesSuite extends FunSuite {
+
+  class MyOtherExamples extends Examples {
+    it("should lead the whole game") {}
+    it("should lead just part of the game") {}
+  }
+
+  test("that duplicate specTexts result in a thrown exception at construction time") {
+
+    new MyOtherExamples
+
+    class MyExamples extends Examples {
+      it("should lead the whole game") {}
+      it("should lead the whole game") {}
+    }
+    intercept[IllegalArgumentException] {
+      new MyExamples  
+    }
+  }
+  test("duplicate testNames should result in an exception when one is in the Examples and the other in the Spec") {
+    class MySpec extends Spec {
+      includeExamples(new MyOtherExamples)
+      it("should lead the whole game") {}
+    }
+    intercept[IllegalArgumentException] {
+      new MySpec  
+    }
+    class MyOtherSpec extends Spec {
+      it("should lead the whole game") {}
+      includeExamples(new MyOtherExamples)
+    }
+    intercept[IllegalArgumentException] {
+      new MyOtherSpec  
+    }
+  }
+}
