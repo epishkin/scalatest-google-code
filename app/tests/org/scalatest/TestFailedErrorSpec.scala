3e57961f514c0283f302f9ea4551291e90b04e4e
bv <bv>
2009-02-16 00:56:19 +0000

    added more tests for TestFailedError

--- app/tests/org/scalatest/TestFailedErrorSpec.scala
+++ app/tests/org/scalatest/TestFailedErrorSpec.scala
@@ -17,7 +17,10 @@ package org.scalatest
 
 class TestFailedErrorSpec extends Spec with ShouldMatchers {
 
+  val baseLineNumber = 20
+
   describe("The FailedTestError") {
+
     it("should give the proper line on fail()") {
       try {
         fail()
@@ -25,11 +28,157 @@ class TestFailedErrorSpec extends Spec with ShouldMatchers {
       catch {
         case e: TestFailedError =>
           e.failedTestCodeFileNameAndLineNumberString match {
-            case Some(s) => s should equal ("TestFailedErrorSpec.scala:23")
+            case Some(s) => s should equal ("TestFailedErrorSpec.scala:" + (baseLineNumber + 6))
             case None => fail("fail() didn't produce a file name and line number string", e)
           }
         case e =>
           fail("fail() didn't produce a TestFailedError", e)
+      }
+    }
+
+    it("should give the proper line on fail(\"message\")") {
+      try {
+        fail("some message")
+      }
+      catch {
+        case e: TestFailedError =>
+          e.failedTestCodeFileNameAndLineNumberString match {
+            case Some(s) => s should equal ("TestFailedErrorSpec.scala:" + (baseLineNumber + 21))
+            case None => fail("fail(\"some message\") didn't produce a file name and line number string", e)
+          }
+        case e =>
+          fail("fail(\"some message\") didn't produce a TestFailedError", e)
+      }
+    }
+
+    it("should give the proper line on fail(throwable)") {
+      try {
+        fail(new RuntimeException)
+      }
+      catch {
+        case e: TestFailedError =>
+          e.failedTestCodeFileNameAndLineNumberString match {
+            case Some(s) => s should equal ("TestFailedErrorSpec.scala:" + (baseLineNumber + 36))
+            case None => fail("fail(throwable) didn't produce a file name and line number string", e)
+          }
+        case e =>
+          fail("fail(throwable) didn't produce a TestFailedError", e)
+      }
+    }
+
+    it("should give the proper line on fail(\"some message\", throwable)") {
+      try {
+        fail("some message", new RuntimeException)
+      }
+      catch {
+        case e: TestFailedError =>
+          e.failedTestCodeFileNameAndLineNumberString match {
+            case Some(s) => s should equal ("TestFailedErrorSpec.scala:" + (baseLineNumber + 51))
+            case None => fail("fail(\"some message\", throwable) didn't produce a file name and line number string", e)
+          }
+        case e =>
+          fail("fail(\"some message\", throwable) didn't produce a TestFailedError", e)
+      }
+    }
+
+    it("should give the proper line on assert(false)") {
+      try {
+        assert(false)
+      }
+      catch {
+        case e: TestFailedError =>
+          e.failedTestCodeFileNameAndLineNumberString match {
+            case Some(s) => s should equal ("TestFailedErrorSpec.scala:" + (baseLineNumber + 66))
+            case None => fail("assert(false) didn't produce a file name and line number string", e)
+          }
+        case e =>
+          fail("assert(false) didn't produce a TestFailedError", e)
+      }
+    }
+
+    it("should give the proper line on assert(false, \"some message\")") {
+      try {
+        assert(false, "some message")
+      }
+      catch {
+        case e: TestFailedError =>
+          e.failedTestCodeFileNameAndLineNumberString match {
+            case Some(s) => s should equal ("TestFailedErrorSpec.scala:" + (baseLineNumber + 81))
+            case None => fail("assert(false, \"some message\") didn't produce a file name and line number string", e)
+          }
+        case e =>
+          fail("assert(false, \"some message\") didn't produce a TestFailedError", e)
+      }
+    }
+
+    it("should give the proper line on assert(1 === 2)") {
+      try {
+        assert(1 === 2)
+      }
+      catch {
+        case e: TestFailedError =>
+          e.failedTestCodeFileNameAndLineNumberString match {
+            case Some(s) => s should equal ("TestFailedErrorSpec.scala:" + (baseLineNumber + 96))
+            case None => fail("assert(1 === 2) didn't produce a file name and line number string", e)
+          }
+        case e =>
+          fail("assert(1 === 2) didn't produce a TestFailedError", e)
+      }
+    }
+
+    it("should give the proper line on assert(1 === 2, \"some message\")") {
+      try {
+        assert(1 === 2, "some message")
+      }
+      catch {
+        case e: TestFailedError =>
+          e.failedTestCodeFileNameAndLineNumberString match {
+            case Some(s) => s should equal ("TestFailedErrorSpec.scala:" + (baseLineNumber + 111))
+            case None => fail("assert(1 === 2, \"some message\") didn't produce a file name and line number string", e)
+          }
+        case e =>
+          fail("assert(1 === 2, \"some message\") didn't produce a TestFailedError", e)
+      }
+    }
+
+    it("should give the proper line on expect(1) { 2 }") {
+      try {
+        expect(1) { 2 }
+      }
+      catch {
+        case e: TestFailedError =>
+          e.failedTestCodeFileNameAndLineNumberString match {
+            case Some(s) => s should equal ("TestFailedErrorSpec.scala:" + (baseLineNumber + 126))
+            case None => fail("expect(1) { 2 } didn't produce a file name and line number string", e)
+          }
+        case e =>
+          fail("expect(1) { 2 } didn't produce a TestFailedError", e)
+      }
+    }
+
+    it("should give the proper line on expect(1, \"some message\") { 2 }") {
+      try {
+        expect(1, "some message") { 2 }
+      }
+      catch {
+        case e: TestFailedError =>
+          e.failedTestCodeFileNameAndLineNumberString match {
+            case Some(s) => s should equal ("TestFailedErrorSpec.scala:" + (baseLineNumber + 141))
+            case None => fail("expect(1, \"some message\") { 2 } didn't produce a file name and line number string", e)
+          }
+        case e =>
+          fail("expect(1, \"some message\") { 2 } didn't produce a TestFailedError", e)
+      }
+    }
+
+    it("bla bla bla") {
+      // fail("message")
+      // fail(new Throwable)
+      // fail("message", new Throwable)
+      // assert(1 === 2, "some message")
+      // assert(1 === 2)
+      expect(1) {
+        2
       }
     }
   }
