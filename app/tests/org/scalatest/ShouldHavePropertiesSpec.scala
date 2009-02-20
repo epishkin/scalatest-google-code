703cfc5df78117890302e5bfce0c247de8d6a559
bv <bv>
2009-02-20 04:58:57 +0000

    dropped the funky approach I used back when I was trying to get have size and have length to short circuit, and added a few property tests

--- app/tests/org/scalatest/ShouldHavePropertiesSpec.scala
+++ app/tests/org/scalatest/ShouldHavePropertiesSpec.scala
@@ -25,7 +25,8 @@ trait BookPropertyVerifiers { this: Matchers =>
   class Book(
     var title: String,
     val author: String,
-    val pubYear: Int
+    val pubYear: Int,
+    val length: Int
   )
 
   class TitleVerifier(expectedValue: String) extends PropertyVerifier[Book, String] {
@@ -69,7 +70,7 @@ class ShouldHavePropertiesSpec extends Spec with ShouldMatchers with Checkers wi
 
     describe("on an object with Scala-style properties") {
 
-      val book = new Book("A Tale of Two Cities", "Dickens", 1859)
+      val book = new Book("A Tale of Two Cities", "Dickens", 1859, 45)
 
       it ("should do nothing if all the properties match") {
         book should have (
@@ -101,7 +102,49 @@ class ShouldHavePropertiesSpec extends Spec with ShouldMatchers with Checkers wi
           )
         }
         assert(caught1.getMessage === "Expected property \"author\" to have value \"Gibson\", but it had value \"Dickens\".")
+
+        val caught2 = intercept[TestFailedException] {
+          book should have (
+            'title ("A Tale of Two Cities"),
+            'author ("Dickens"),
+            'pubYear (1959)
+          )
+        }
+        assert(caught2.getMessage === "Expected property \"pubYear\" to have value 1959, but it had value 1859.")
       }
+
+      it ("should throw TestFailedException if there's just one property and it doesn't match") {
+
+        val caught1 = intercept[TestFailedException] {
+          book should have (author ("Gibson"))
+        }
+        assert(caught1.getMessage === "Expected property \"author\" to have value \"Gibson\", but it had value \"Dickens\".")
+      }
+
+      it ("should throw TestFailedException if there's just one property and it doesn't match, when using a symbol") {
+
+        val caught1 = intercept[TestFailedException] {
+          book should have ('author ("Gibson"))
+        }
+        assert(caught1.getMessage === "Expected property \"author\" to have value \"Gibson\", but it had value \"Dickens\".")
+      }
+
+/*
+      it ("should work with length not a symbol without anything special, in case someone forgets you don't need the parens with length") {
+
+        val caught1 = intercept[TestFailedException] {
+          book should have (length (43))
+        }
+        assert(caught1.getMessage === "Expected property \"length\" to have value 43, but it had value 45.")
+      }
+*/
+
+      /*
+      This does not compile, which is what I want
+      it ("should not compile if you don't enter any verifiers") {
+        book should have ()
+      }
+      */
     }
   }
 }
