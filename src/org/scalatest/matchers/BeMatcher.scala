1eef0245964eef31506da877d35d7ed21e9ddca5
bv <bv>
2009-02-27 06:02:57 +0000

    dropped some wrong scaladoc comments

--- src/org/scalatest/matchers/BeMatcher.scala
+++ src/org/scalatest/matchers/BeMatcher.scala
@@ -16,69 +16,12 @@
 package org.scalatest.matchers
 
 /**
- * Trait extended by objects that can match a value of the specified type. The value to match is
- * passed to the matcher's <code>apply</code> method. The result is a <code>MatchResult</code>.
- * A matcher is, therefore, a function from the specified type, <code>T</code>, to a <code>MatchResult</code>.
- *
- * <p>
- * <code>Matcher</code> is contravariant in its type parameter, <code>T</code>, to make its use more flexible.
- * As an example, consider the hierarchy:
- * </p>
- *
- * <pre>
- * class Fruit
- * class Orange extends Fruit
- * class ValenciaOrange extends Orange
- * </pre>
- *
- * <p>
- * Given an orange:
- * </p>
- *
- * <pre>
- * val orange = Orange
- * </pre>
- *
- * <p>
- * The expression "<code>orange should</code>" will, via an implicit conversion in <code>ShouldMatchers</code>,
- * result in an object that has a <code>should</code>
- * method that takes a <code>Matcher[Orange]</code>. If the static type of the matcher being passed to <code>should</code> is
- * <code>Matcher[Valencia]</code> it shouldn't (and won't) compile. The reason it shouldn't compile is that
- * the left value is an <code>Orange</code>, but not necessarily a <code>Valencia</code>, and a
- * <code>Matcher[Valencia]</code> only knows how to match against a <code>Valencia</code>. The reason
- * it won't compile is given that <code>Matcher</code> is contravariant in its type parameter, <code>T</code>, a
- * <code>Matcher[Valencia]</code> is <em>not</em> a subtype of <code>Matcher[Orange]</code>.
- * </p>
- *
- * <p>
- * By contrast, if the static type of the matcher being passed to <code>should</code> is <code>Matcher[Fruit]</code>,
- * it should (and will) compile. The reason is should compile is that given the left value is an <code>Orange</code>,
- * it is also a <code>Fruit</code>, and a <code>Matcher[Fruit]</code> knows how to match against <code>Fruit</code>s.
- * The reason it will compile is that given  that <code>Matcher</code> is contravariant in its type parameter, <code>T</code>, a
- * <code>Matcher[Fruit]</code> is indeed a subtype of <code>Matcher[Orange]</code>.
- * </p>
- *
+ * TODO
  */
 trait BeMatcher[-T] extends Function1[T, MatchResult] {
 
   /**
-   * Check to see if the specified object, <code>left</code>, matches, and report the result in
-   * the returned <code>MatchResult</code>. The parameter is named <code>left</code>, because it is
-   * usually the value to the left of a <code>should</code> or <code>must</code> invocation. For example,
-   * in:
-   *
-   * <pre>
-   * list should equal (List(1, 2, 3))
-   * </pre>
-   *
-   * The <code>equal (List(1, 2, 3))</code> expression returns a matcher that holds a reference to the
-   * right value, <code>List(1, 2, 3)</code>. The <code>should</code> method invokes <code>apply</code>
-   * on this matcher, passing in <code>list</code>, which is therefore the "<code>left</code>" value. The
-   * matcher will compare the <code>list</code> (the <code>left</code> value) with <code>List(1, 2, 3)</code> (the right
-   * value), and report the result in the returned <code>MatchResult</code>.
-   *
-   * @param left the value against which to match
-   * @return the <code>MatchResult</code> that represents the result of the match
+   * TODO
    */
   def apply(left: T): MatchResult
 }
