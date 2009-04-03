74664744098ab783c7dbd9c8c83ce7e3e2d6b523
gcberger <gcberger>
2009-04-03 16:54:46 +0000

    merged george-3 branch into trunk - contains scalac fork and other build updates

--- app/src/org/scalatest/tools/Runner.scala
+++ app/src/org/scalatest/tools/Runner.scala
@@ -341,9 +341,26 @@ import org.scalatest.testng.TestNGWrapperSuite
  * @author Josh Cough
  */
 object Runner {
-
   private val RUNNER_JFRAME_START_X: Int = 150
   private val RUNNER_JFRAME_START_Y: Int = 100
+  private val passFailReporter = new PassFailReporter
+
+  //
+  // We always include a PassFailReporter on runs in order to determine
+  // whether or not all tests passed.
+  //
+  class PassFailReporter extends Reporter {
+    var fail = false
+    var allTestsCompleted = false
+
+    override def testFailed(report: Report)   = { fail = true }
+    override def runAborted(report: Report)   = { fail = true }
+    override def suiteAborted(report: Report) = { fail = true }
+
+    override def runCompleted() = { allTestsCompleted = true }
+  }
+  def allTestsPassed =
+    passFailReporter.allTestsCompleted && !passFailReporter.fail
 
   // TODO: I don't think I'm enforcing that properties can't start with "org.scalatest"
   // TODO: I don't think I'm handling rejecting multiple -f/-r with the same arg. -f fred.txt -f fred.txt should
@@ -353,6 +370,13 @@ object Runner {
    * Runs a suite of tests, with optional GUI. See the main documentation for this singleton object for the details.
    */
   def main(args: Array[String]) {
+    runTests(args)
+  }
+
+  //
+  // Returns true if all tests passed.
+  //
+  def runTests(args: Array[String]): boolean = {
 
     checkArgsForValidity(args) match {
       case Some(s) => {
@@ -440,6 +464,7 @@ object Runner {
         }
       }
     }
+    allTestsPassed
   }
 
   // Returns an Option[String]. Some is an error message. None means no error.
@@ -866,8 +891,8 @@ object Runner {
     }
 
     val reporterSeq =
-      for (spec <- reporterSpecs)
-        yield getReporterFromSpec(spec)
+      (for (spec <- reporterSpecs)
+        yield getReporterFromSpec(spec)) ++ List(passFailReporter)
 
     val fullReporterList: List[Reporter] =
       graphicReporter match {
