9b3712e158cb7508ca5bc7dc64f688fdd2fee9d0
bv <bv>
2009-05-13 19:41:41 +0000

    added the Event hierarchy and some tests. Originally did this in the reporter refactor branch

---
+++ app/src/org/scalatest/events/Summary.scala
@@ -0,0 +1,16 @@
+package org.scalatest.events
+
+/**
+ * Class each of whose instances hold summary information about one ScalaTest run.
+ *
+ * @param testsSucceededCount the number of tests that were reported as succeeded during the run
+ * @param testsFailedCount the number of tests that were reported as failed during the run
+ * @param testsIgnoredCount the number of tests that were were reported as ignored during the run
+ * @param testsPendingCount the number of tests that were reported as pending during the run
+ */
+final case class Summary private (testsSucceededCount: Int, testsFailedCount: Int, testsIgnoredCount: Int, testsPendingCount: Int)
+
+/**
+ * Companion object for case class <a href="Summary.html"><code>Summary</code></a>.
+ */
+object Summary
