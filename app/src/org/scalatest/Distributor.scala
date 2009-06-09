f2ba2a037f8040281f7a0fde1b837a7290e9af05
bv <bv>
2009-06-09 14:54:12 +0000

    added comment

--- app/src/org/scalatest/Distributor.scala
+++ app/src/org/scalatest/Distributor.scala
@@ -55,3 +55,53 @@ trait Distributor {
    */
   def put(suite: Suite)
 }
+/*
+   Could make this a function too. Would simply be (Suite) => Unit. Could name the parameter stopRequested
+   Then the code would be:
+
+   distribute(suite)
+
+   instead of:
+
+   distributor.put(suite)
+
+   or, could bump it up to an execution strategy
+
+   execute(suite)
+
+   I don't think so.
+
+  Could rename it DistributeFunction instead of Distributor, so they'd write:
+  distribute: DistributeFunction
+
+  Could I call it DistributeFun? or Distribute
+  distribute: DistributeFun
+
+  I use Fun elsewhere. In FunSuite. So maybe.
+
+  The distribute method needs the ordinal too:
+  trait DistributeFunction {
+
+    def apply(suite: Suite, nextOrdinal: Ordinal)
+  }
+
+ I wonder if I could call these Stopper, Distributor, Reporter, etc., then make type aliases for Suite.
+ val type ReportFunction = Reporter
+ val type DistributeFunction = Distributor
+ val type FilterFunction = Filter
+ val type Function = Filter
+
+  Maybe just leave the names the same and make the names verbs:
+  testName: Option[String], report: Reporter, stopRequested: Stopper, filter: Filter, distributor: Option[Distributor], goodies: Map[String, Set[String]]
+
+  Calling code is ugly. Looks like report=, instead of reporter= But really, this is the framework doing this usually, and you don't have to use that param notation.
+  Frankly, I think it should be for inside the method. Though that's wierd. It should be for users, and here, the users are implementing this method, not calling it.
+  distributor is a noun, whereas report, stopRequested, filter are ready to go verby functionnames, because distributor is optional
+  distributor match {
+    case Some(distribute) => distribute(suite, nextOrdinal)
+    case None => bla bla bla
+  }
+
+  Can have a DisposableReporter with a dispose() method. Can then just do some pattern matching when the time comes to dispose. That way the parameter type can have
+  the strong name of Reporter.
+*/
