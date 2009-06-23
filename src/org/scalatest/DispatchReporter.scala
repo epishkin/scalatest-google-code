7b16be13d1eb3cd83aeb526fe7dc320a18de3850
bv <bv>
2009-06-23 03:03:37 +0000

    Put the stuff in DispatchReporter to count tests succeeded, failed, pending, etc., and changed PrintReporter so that it uses it for RunCompleted and RunStopped. (Still need to do RunAborted in PrintReporter). Checking in an interim workingish version

--- src/org/scalatest/DispatchReporter.scala
+++ src/org/scalatest/DispatchReporter.scala
@@ -21,7 +21,7 @@ import scala.actors.Actor.actor
 import scala.actors.Actor.loop
 import scala.actors.Actor.receive
 import java.io.PrintStream
-import org.scalatest.events.Event
+import org.scalatest.events._
 import DispatchReporter.propagateDispose
 
 /**
@@ -44,13 +44,107 @@ private[scalatest] class DispatchReporter(val reporters: List[Reporter], out: Pr
   private case object Dispose
 
   private val julia = actor {
+
     var alive = true // local variable, right? Only used by the Actor's thread, so no need for synchronization
+
+    class Counter {
+      var testsSucceededCount = 0
+      var testsFailedCount = 0
+      var testsIgnoredCount = 0
+      var testsPendingCount = 0
+      var suitesAbortedCount = 0
+    }
+
+    val counterMap = scala.collection.mutable.Map[Int, Counter]()
+
+    def incrementCount(event: Event, f: (Counter) => Unit) {
+      val runStamp = event.ordinal.runStamp
+      if (counterMap.contains(runStamp)) {
+        val counter = counterMap(runStamp)
+        f(counter)
+      }
+      else {
+        val counter = new Counter
+        f(counter)
+        counterMap(runStamp) = counter
+      }
+    }
+
+    // If None, that means don't update the summary so forward the old event. If Some,
+    // create a new event with everything the same except the old summary replaced by the new one
+    def updatedSummary(oldSummary: Option[Summary], ordinal: Ordinal): Option[Summary] = {
+      oldSummary match {
+        case Some(_) => None // pass old summary through if it is already defined
+        case None =>
+          if (counterMap.contains(ordinal.runStamp)) {
+            // Update the RunAborted so that it is the same except it has a new Some(Summary)
+            val counter = counterMap(ordinal.runStamp)
+            Some(
+              Summary(
+                counter.testsSucceededCount,
+                counter.testsFailedCount,
+                counter.testsIgnoredCount,
+                counter.testsPendingCount,
+                counter.suitesAbortedCount
+              )
+            )
+          }
+          else None // Also pass the old None summary through if it isn't in the counterMap
+      }
+    }
+
     while (alive) {
       receive {
         case event: Event => 
           try {
+            // The event will only actually be updated if it it is a RunCompleted/Aborted/Stopped event with None
+            // as its summary and its runstamp has a counter entry. In that case, it will be given a Summary taken
+            // from the counter. (And the counter will be removed from the counterMap.) These are counted here, because
+            // they need to be counted on this side of any FilterReporters that may be in place. (In early versions of
+            // ScalaTest, these were wrongly being counted by the reporters themselves, so if a FilterReporter filtered
+            // out TestSucceeded events, then they just weren't being counted.
+            val updatedEvent =
+              event match {
+
+                case _: RunStarting => counterMap(event.ordinal.runStamp) = new Counter; event
+
+                case _: TestSucceeded => incrementCount(event, _.testsSucceededCount += 1); event
+                case _: TestFailed => incrementCount(event, _.testsFailedCount += 1); event
+                case _: TestIgnored => incrementCount(event, _.testsIgnoredCount += 1); event
+                case _: TestPending => incrementCount(event, _.testsPendingCount += 1); event
+                case _: SuiteAborted => incrementCount(event, _.suitesAbortedCount += 1); event
+
+                case oldRunCompleted @ RunCompleted(ordinal, duration, summary, formatter, payload, threadName, timeStamp) =>
+                  updatedSummary(summary, ordinal) match {
+                    case None => oldRunCompleted
+                    case newSummary @ Some(_) =>
+                      counterMap.removeKey(ordinal.runStamp)
+                      // Update the RunCompleted so that it is the same except it has a new Some(Summary)
+                      RunCompleted(ordinal, duration, newSummary, formatter, payload, threadName, timeStamp)
+                  }
+      
+                case oldRunStopped @ RunStopped(ordinal, duration, summary, formatter, payload, threadName, timeStamp) =>
+                  updatedSummary(summary, ordinal) match {
+                    case None => oldRunStopped
+                    case newSummary @ Some(_) =>
+                      counterMap.removeKey(ordinal.runStamp)
+                      // Update the RunStopped so that it is the same except it has a new Some(Summary)
+                      RunStopped(ordinal, duration, newSummary, formatter, payload, threadName, timeStamp)
+                  }
+                
+                case oldRunAborted @ RunAborted(ordinal, message, throwable, duration, summary, formatter, payload, threadName, timeStamp) => 
+                  updatedSummary(summary, ordinal) match {
+                    case None => oldRunAborted
+                    case newSummary @ Some(_) =>
+                      counterMap.removeKey(ordinal.runStamp)
+                      // Update the RunAborted so that it is the same except it has a new Some(Summary)
+                      RunAborted(ordinal, message, throwable, duration, newSummary, formatter, payload, threadName, timeStamp)
+                  }
+                
+                case _ => event
+              }
             for (report <- reporters)
-              report(event)
+              report(updatedEvent)
           }
           catch {
             case e: Exception => 
