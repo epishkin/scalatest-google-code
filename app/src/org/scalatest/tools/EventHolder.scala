9a0a08e397750b24e3014e2fec67c3b4630732a4
bv <bv>
2009-06-30 02:22:23 +0000

    added summary info to GUI

--- app/src/org/scalatest/tools/EventHolder.scala
+++ app/src/org/scalatest/tools/EventHolder.scala
@@ -25,13 +25,16 @@ import EventHolder.suiteAndTestName
  * @author Bill Venners
  */
 private[tools] class EventHolder(val event: Event, val message: Option[String], val throwable: Option[Throwable],
-    val rerunner: Option[Rerunner], val isRerun: Boolean) {
+    val rerunner: Option[Rerunner], val summary: Option[Summary], val isRerun: Boolean) {
 
-  if (event == null || message == null || throwable == null || rerunner == null)
+  if (event == null || message == null || throwable == null || rerunner == null || summary == null)
     throw new NullPointerException()
  
   def this(event: Event, message: Option[String], throwable: Option[Throwable],
-      rerunner: Option[Rerunner]) = this(event, message, throwable, rerunner, false)
+      rerunner: Option[Rerunner]) = this(event, message, throwable, rerunner, None, false)
+
+  def this(event: Event, message: Option[String], throwable: Option[Throwable],
+      rerunner: Option[Rerunner], summary: Option[Summary]) = this(event, message, throwable, rerunner, summary, false)
 
   override def toString = {
     event.formatter match {
