d80463d4b27d887ed2dbac102fac73eeea6a12cc
bv <bv>
2009-07-08 21:33:16 +0000

    init ver

---
+++ app/tests/org/scalatest/WordSpecSpec.scala
@@ -0,0 +1,328 @@
+/*
+ * Copyright 2001-2008 Artima, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.scalatest
+
+import org.scalatest.events._
+import org.scalatest.mytags._
+
+class WordSpecSpec extends Spec with SharedHelpers with GivenWhenThen {
+
+  describe("A Spec") {
+
+    it("should return the test names in registration order from testNames") {
+
+      val a = new WordSpec {
+        it("should test this") {}
+        it("should test that") {}
+      }
+
+      expect(List("should test this", "should test that")) {
+        a.testNames.elements.toList
+      }
+
+      val b = new WordSpec {}
+
+      expect(List[String]()) {
+        b.testNames.elements.toList
+      }
+
+      val c = new WordSpec {
+        it("should test that") {}
+        it("should test this") {}
+      }
+
+      expect(List("should test that", "should test this")) {
+        c.testNames.elements.toList
+      }
+
+      val d = new WordSpec {
+        "A Tester" can {
+          it("test that") {}
+          it("test this") {}
+        }
+      }
+
+      expect(List("A Tester can test that", "A Tester can test this")) {
+        d.testNames.elements.toList
+      }
+
+      val e = new WordSpec {
+        "A Tester" can {
+          it("test this") {}
+          it("test that") {}
+        }
+      }
+
+      expect(List("A Tester can test this", "A Tester can test that")) {
+        e.testNames.elements.toList
+      }
+    }
+
+/*
+    it("should throw DuplicateTestNameException if a duplicate test name registration is attempted") {
+      
+      intercept[DuplicateTestNameException] {
+        new WordSpec {
+          it("should test this") {}
+          it("should test this") {}
+        }
+      }
+      intercept[DuplicateTestNameException] {
+        new WordSpec {
+          it("should test this") {}
+          ignore("should test this") {}
+        }
+      }
+      intercept[DuplicateTestNameException] {
+        new WordSpec {
+          ignore("should test this") {}
+          ignore("should test this") {}
+        }
+      }
+      intercept[DuplicateTestNameException] {
+        new WordSpec {
+          ignore("should test this") {}
+          it("should test this") {}
+        }
+      }
+    }
+    describe("(with info calls)") {
+      class InfoInsideTestSpec extends WordSpec {
+        val msg = "hi there, dude"
+        val testName = "test name"
+        it(testName) {
+          info(msg)
+        }
+      }
+      // In a Spec, any InfoProvided's fired during the test should be cached and sent out after the test has
+      // suceeded or failed. This makes the report look nicer, because the info is tucked under the "specifier'
+      // text for that test.
+      it("should, when the info appears in the code of a successful test, report the info after the TestSucceeded") {
+        val spec = new InfoInsideTestSpec
+        val (infoProvidedIndex, testStartingIndex, testSucceededIndex) =
+          getIndexesForInformerEventOrderTests(spec, spec.testName, spec.msg)
+        assert(testSucceededIndex < infoProvidedIndex)
+      }
+      class InfoBeforeTestSpec extends WordSpec {
+        val msg = "hi there, dude"
+        val testName = "test name"
+        info(msg)
+        it(testName) {}
+      }
+      it("should, when the info appears in the body before a test, report the info before the test") {
+        val spec = new InfoBeforeTestSpec
+        val (infoProvidedIndex, testStartingIndex, testSucceededIndex) =
+          getIndexesForInformerEventOrderTests(spec, spec.testName, spec.msg)
+        assert(infoProvidedIndex < testStartingIndex)
+        assert(testStartingIndex < testSucceededIndex)
+      }
+      it("should, when the info appears in the body after a test, report the info after the test runs") {
+        val msg = "hi there, dude"
+        val testName = "test name"
+        class MySpec extends WordSpec {
+          it(testName) {}
+          info(msg)
+        }
+        val (infoProvidedIndex, testStartingIndex, testSucceededIndex) =
+          getIndexesForInformerEventOrderTests(new MySpec, testName, msg)
+        assert(testStartingIndex < testSucceededIndex)
+        assert(testSucceededIndex < infoProvidedIndex)
+      }
+      it("should throw an IllegalStateException when info is called by a method invoked after the suite has been executed") {
+        class MySpec extends WordSpec {
+          callInfo() // This should work fine
+          def callInfo() {
+            info("howdy")
+          }
+          it("howdy also") {
+            callInfo() // This should work fine
+          }
+        }
+        val spec = new MySpec
+        val myRep = new EventRecordingReporter
+        spec.run(None, myRep, new Stopper {}, Filter(), Map(), None, new Tracker)
+        intercept[IllegalStateException] {
+          spec.callInfo()
+        }
+      }
+      it("should send an InfoProvided with an IndentedText formatter with level 1 when called outside a test") {
+        val spec = new InfoBeforeTestSpec
+        val indentedText = getIndentedTextFromInfoProvided(spec)
+        assert(indentedText === IndentedText("+ " + spec.msg, spec.msg, 1))
+      }
+      it("should send an InfoProvided with an IndentedText formatter with level 2 when called within a test") {
+        val spec = new InfoInsideTestSpec
+        val indentedText = getIndentedTextFromInfoProvided(spec)
+        assert(indentedText === IndentedText("  + " + spec.msg, spec.msg, 2))
+      }
+    }
+    it("should return registered tags, including ignore tags, from the tags method") {
+
+      val a = new WordSpec {
+        ignore("should test this") {}
+        it("should test that") {}
+      }
+      expect(Map("should test this" -> Set("org.scalatest.Ignore"))) {
+        a.tags
+      }
+
+      val b = new WordSpec {
+        it("should test this") {}
+        ignore("should test that") {}
+      }
+      expect(Map("should test that" -> Set("org.scalatest.Ignore"))) {
+        b.tags
+      }
+
+      val c = new WordSpec {
+        ignore("should test this") {}
+        ignore("should test that") {}
+      }
+      expect(Map("should test this" -> Set("org.scalatest.Ignore"), "should test that" -> Set("org.scalatest.Ignore"))) {
+        c.tags
+      }
+
+      val d = new WordSpec {
+        it("should test this") {}
+        it("should test that") {}
+      }
+      expect(Map()) {
+        d.tags
+      }
+
+      val e = new WordSpec {
+        it("should test this", SlowAsMolasses) {}
+        ignore("should test that", SlowAsMolasses) {}
+      }
+      expect(Map("should test this" -> Set("org.scalatest.SlowAsMolasses"), "should test that" -> Set("org.scalatest.Ignore", "org.scalatest.SlowAsMolasses"))) {
+        e.tags
+      }
+
+      val f = new WordSpec {}
+      expect(Map()) {
+        f.tags
+      }
+
+      val g = new WordSpec {
+        it("should test this", SlowAsMolasses, WeakAsAKitten) {}
+        it("should test that", SlowAsMolasses) {}
+      }
+      expect(Map("should test this" -> Set("org.scalatest.SlowAsMolasses", "org.scalatest.WeakAsAKitten"), "should test that" -> Set("org.scalatest.SlowAsMolasses"))) {
+        g.tags
+      }
+    }
+
+    describe("(when a nesting rule has been violated)") {
+
+      it("should, if they call a describe from within an it clause, result in a TestFailedException when running the test") {
+
+        class MySpec extends WordSpec {
+          it("should blow up") {
+            describe("in the wrong place, at the wrong time") {
+            }
+          }
+        }
+
+        val spec = new MySpec
+        ensureTestFailedEventReceived(spec, "should blow up")
+      }
+      it("should, if they call a describe with a nested it from within an it clause, result in a TestFailedException when running the test") {
+
+        class MySpec extends WordSpec {
+          it("should blow up") {
+            describe("in the wrong place, at the wrong time") {
+              it("should never run") {
+                assert(1 === 2)
+              }
+            }
+          }
+        }
+
+        val spec = new MySpec
+        ensureTestFailedEventReceived(spec, "should blow up")
+      }
+      it("should, if they call a nested it from within an it clause, result in a TestFailedException when running the test") {
+
+        class MySpec extends WordSpec {
+          it("should blow up") {
+            it("should never run") {
+              assert(1 === 2)
+            }
+          }
+        }
+
+        val spec = new MySpec
+        ensureTestFailedEventReceived(spec, "should blow up")
+      }
+      it("should, if they call a nested it with tags from within an it clause, result in a TestFailedException when running the test") {
+
+        class MySpec extends WordSpec {
+          it("should blow up") {
+            it("should never run", SlowAsMolasses) {
+              assert(1 === 2)
+            }
+          }
+        }
+
+        val spec = new MySpec
+        ensureTestFailedEventReceived(spec, "should blow up")
+      }
+      it("should, if they call a describe with a nested ignore from within an it clause, result in a TestFailedException when running the test") {
+
+        class MySpec extends WordSpec {
+          it("should blow up") {
+            describe("in the wrong place, at the wrong time") {
+              ignore("should never run") {
+                assert(1 === 2)
+              }
+            }
+          }
+        }
+
+        val spec = new MySpec
+        ensureTestFailedEventReceived(spec, "should blow up")
+      }
+      it("should, if they call a nested ignore from within an it clause, result in a TestFailedException when running the test") {
+
+        class MySpec extends WordSpec {
+          it("should blow up") {
+            ignore("should never run") {
+              assert(1 === 2)
+            }
+          }
+        }
+
+        val spec = new MySpec
+        ensureTestFailedEventReceived(spec, "should blow up")
+      }
+      it("should, if they call a nested ignore with tags from within an it clause, result in a TestFailedException when running the test") {
+
+        class MySpec extends WordSpec {
+          it("should blow up") {
+            ignore("should never run", SlowAsMolasses) {
+              assert(1 === 2)
+            }
+          }
+        }
+
+        val spec = new MySpec
+        ensureTestFailedEventReceived(spec, "should blow up")
+      }
+    }
+*/
+  }
+}
+
