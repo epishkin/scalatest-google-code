fc8a31bf4d0209665328d53a3a1fba9238b80d0b
bv <bv>
2009-07-11 01:14:13 +0000

    move Suite in fixture dir into fixture package

--- src/org/scalatest/fixture/Suite.scala
+++ src/org/scalatest/fixture/Suite.scala
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.scalatest.goodies
+package org.scalatest.fixture
 
 import collection.immutable.TreeSet
 import java.lang.reflect.{InvocationTargetException, Method, Modifier}
