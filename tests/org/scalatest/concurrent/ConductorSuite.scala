cc4932cb5a6dc805920ca1e450fa0a564fbf8e63
bv <bv>
2009-07-16 02:43:23 +0000

    added more commented out pending tests to ConductorSuite

--- tests/org/scalatest/concurrent/ConductorSuite.scala
+++ tests/org/scalatest/concurrent/ConductorSuite.scala
@@ -213,4 +213,25 @@ class ConductorSuite extends FunSuite with ConductorMethods with ShouldMatchers 
 
   // test("if whenFinished is called twice on the same conductor, an IllegalStateException is thrown that explains it" +
   //        "can only be called once")
+
+  // One issue is whther ConductorMethods should have a conductTest on it at all. Possibly not, but the trouble
+  // is then that Conductor does have this method, and ConductorMethods wouldn't be being completely honest.
+  // Also there would be something you could call in ConductorFixture that you couldn't call in ConductorMethods.
+  // So my feeling is go ahead and add it to be consistent, but I'm not sure. Please check also to see if there are any
+  // other public methods on Conductor that are not in ConductorMethods. If this is the only one, then probably we
+  // should add it.
+
+  // For ConductorMethods (probably in a ConductorMethodsSuite)
+  // test("if conductTest is called from within the test itself, the test still succeeds (in other words," +
+  //        "ConductorMethods doesn't call it if testWasConducted is true.")
+
+  // test("if conductTest is not called from within the test itself, the test still executes (because
+  //       "ConductorMethods calls it given testWasConducted is false")
+
+  // ConductorFixture  (probably in a ConductorMethodsSuite)
+  // test("if conductTest is called from within the test itself, the test still succeeds (in other words," +
+  //        "ConductorFixture doesn't call it if testWasConducted is true.")
+
+  // test("if conductTest is not called from within the test itself, the test still executes (because
+  //       "ConductorFixture calls it given testWasConducted is false")
 }
