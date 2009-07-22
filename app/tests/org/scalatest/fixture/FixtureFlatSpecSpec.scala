3f5388d0fa883d55ff3037c6c26cadbb557cb5a2
bv <bv>
2009-07-22 23:31:02 +0000

    done testing the expectedTestCount method

--- app/tests/org/scalatest/fixture/FixtureFlatSpecSpec.scala
+++ app/tests/org/scalatest/fixture/FixtureFlatSpecSpec.scala
@@ -896,5 +896,60 @@ class FixtureFlatSpecSpec extends org.scalatest.Spec with PrivateMethodTester wi
       assert(!k.theTestThatCalled)
       assert(!k.theTestTheOtherCalled)
     }
+
+    it("should return the correct test count from its expectedTestCount method") {
+
+      val a = new FlatSpec with SimpleWithFixture {
+        type Fixture = String
+        def withFixture(fun: String => Unit) { fun("hi") }
+        it should "test this" in { fixture => }
+        it should "test that" in { fixture => }
+      }
+      assert(a.expectedTestCount(Filter()) === 2)
+
+      val b = new FlatSpec with SimpleWithFixture {
+        type Fixture = String
+        def withFixture(fun: String => Unit) { fun("hi") }
+        ignore should "test this" in { fixture => }
+        it should "test that" in { fixture => }
+      }
+      assert(b.expectedTestCount(Filter()) === 1)
+
+      val c = new FlatSpec with SimpleWithFixture {
+        type Fixture = String
+        def withFixture(fun: String => Unit) { fun("hi") }
+        it should "test this" taggedAs(mytags.FastAsLight) in { fixture => }
+        it should "test that" in { fixture => }
+      }
+      assert(c.expectedTestCount(Filter(Some(Set("org.scalatest.FastAsLight")), Set())) === 1)
+      assert(c.expectedTestCount(Filter(None, Set("org.scalatest.FastAsLight"))) === 1)
+
+      val d = new FlatSpec with SimpleWithFixture {
+        type Fixture = String
+        def withFixture(fun: String => Unit) { fun("hi") }
+        it should "test this" taggedAs(mytags.FastAsLight, mytags.SlowAsMolasses) in { fixture => }
+        it should "test that" taggedAs(mytags.SlowAsMolasses) in { fixture => }
+        it should "test the other thing" in { fixture => }
+      }
+      assert(d.expectedTestCount(Filter(Some(Set("org.scalatest.FastAsLight")), Set())) === 1)
+      assert(d.expectedTestCount(Filter(Some(Set("org.scalatest.SlowAsMolasses")), Set("org.scalatest.FastAsLight"))) === 1)
+      assert(d.expectedTestCount(Filter(None, Set("org.scalatest.SlowAsMolasses"))) === 1)
+      assert(d.expectedTestCount(Filter()) === 3)
+
+      val e = new FlatSpec with SimpleWithFixture {
+        type Fixture = String
+        def withFixture(fun: String => Unit) { fun("hi") }
+        it should "test this" taggedAs(mytags.FastAsLight, mytags.SlowAsMolasses) in { fixture => }
+        it should "test that" taggedAs(mytags.SlowAsMolasses) in { fixture => }
+        ignore should "test the other thing" in { fixture => }
+      }
+      assert(e.expectedTestCount(Filter(Some(Set("org.scalatest.FastAsLight")), Set())) === 1)
+      assert(e.expectedTestCount(Filter(Some(Set("org.scalatest.SlowAsMolasses")), Set("org.scalatest.FastAsLight"))) === 1)
+      assert(e.expectedTestCount(Filter(None, Set("org.scalatest.SlowAsMolasses"))) === 0)
+      assert(e.expectedTestCount(Filter()) === 2)
+
+      val f = new SuperSuite(List(a, b, c, d, e))
+      assert(f.expectedTestCount(Filter()) === 10)
+    }
   }
 }
