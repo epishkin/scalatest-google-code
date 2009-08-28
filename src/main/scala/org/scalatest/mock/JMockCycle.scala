3a66455ec115eecdc1ea4d60a5363635fc44c206
bv <bv>
2009-08-28 07:05:54 +0000

    added a test for doing withArg and realized it was too much for 1.0. Added some documentation for JMockCycle

--- src/main/scala/org/scalatest/mock/JMockCycle.scala
+++ src/main/scala/org/scalatest/mock/JMockCycle.scala
@@ -20,6 +20,138 @@ import org.jmock.api.ExpectationError
 import org.jmock.{Expectations, Mockery}
 import scala.reflect.Manifest
 
+/**
+ * Class that wraps and manages the lifecycle of a single <code>org.jmock.Mockery</code> context object,
+ * provides some basic syntax sugar for using <a href="http://www.jmock.org/" target="_blank">JMock</a>
+ * in Scala.
+ *
+ * <p>
+ * Using the JMock API directly, you first need a <code>Mockery</code> context object:
+ * </p>
+ *
+ * <pre>
+ * val context = new Mockery
+ * </pre>
+ *
+ * <p>
+ * When using this class, you would instead create an instance of this class (which will create and
+ * wrap a <code>Mocker</code> object) and import its members, like this:
+ * </p>
+ *
+ * <pre>
+ * val cycle = new JMockCycle
+ * import cycle._
+ * </pre>
+ *
+ * <p>
+ * Using the JMock API directly, you would create a mock object like this:
+ * </p>
+ *
+ * <pre>
+ * val mockCollaborator = context.mock(classOf[Collaborator])
+ * </pre>
+ *
+ * <p>
+ * Having imported the members of an instance of this class, you can shorten that to:
+ * </p>
+ *
+ * <pre>
+ * val mockCollaborator = mock[Collaborator]
+ * </pre>
+ *
+ * <p>
+ * After creating mocks, you set expectations on them, using syntax like this:
+ * </p>
+ *
+ * <pre>
+ * context.checking(
+ *   new Expectations() {
+ *     oneOf (mockCollaborator).documentAdded("Document")
+ *     exactly(3).of (mockCollaborator).documentChanged("Document")
+ *    }
+ *  )
+ * </pre>
+ *
+ * <p>
+ * Having imported the members of an instance of this class, you can shorten this step to:
+ * </p>
+ *
+ * <pre>
+ * expecting { e => import e._
+ *   oneOf (mockCollaborator).documentAdded("Document")
+ *   exactly(3).of (mockCollaborator).documentChanged("Document")
+ * }
+ * </pre>
+ *
+ * <p>
+ * The <code>expecting</code> method will create a new <code>Expectations</code> object, pass it into
+ * the function you provide, which sets the expectations. After the function returns, the <code>expecting</code>
+ * method will pass the <code>Expectations</code> object to the <code>checking</code>
+ * method of its internal <code>Mockery</code> context.
+ * </p>
+ *
+ * <p>
+ * Once you've set expectations on the mock objects, when using the JMock API directly, you use the mock, then invoke
+ * <code>assertIsSatisfied</code> on the <code>Mockery</code> context to make sure the mock
+ * was used in accordance with the expectations you set on it. Here's how that looks:
+ * </p>
+ *
+ * <pre>
+ * classUnderTest.addDocument("Document", new Array[Byte](0))
+ * classUnderTest.addDocument("Document", new Array[Byte](0))
+ * classUnderTest.addDocument("Document", new Array[Byte](0))
+ * classUnderTest.addDocument("Document", new Array[Byte](0))
+ * context.assertIsSatisfied()
+ * </pre>
+ *
+ * <p>
+ * This class enables you to use the following, more declarative syntax instead:
+ * </p>
+ *
+ * <pre>
+ * whenExecuting {
+ *   classUnderTest.addDocument("Document", new Array[Byte](0))
+ *   classUnderTest.addDocument("Document", new Array[Byte](0))
+ *   classUnderTest.addDocument("Document", new Array[Byte](0))
+ *   classUnderTest.addDocument("Document", new Array[Byte](0))
+ * }
+ * </pre>
+ *
+ * <p>
+ * The <code>whenExecuting</code> method will execute the passed function, then
+ * invoke <code>assertIsSatisfied</code> on its internal <code>Mockery</code>
+ * context object.
+ * </p>
+ *
+ * <p>
+ * To summarize, here's what a typical test using <code>JMockCycle</code> looks like:
+ * </p>
+ *
+ * <pre>
+ * val cycle = new JMockCycle
+ * import cycle._
+ *
+ * val mockCollaborator = mock[Collaborator]
+ *
+ * expecting { e => import e._
+ *   oneOf (mockCollaborator).documentAdded("Document")
+ *   exactly(3).of (mockCollaborator).documentChanged("Document")
+ * }
+ *
+ * whenExecuting {
+ *   classUnderTest.addDocument("Document", new Array[Byte](0))
+ *   classUnderTest.addDocument("Document", new Array[Byte](0))
+ *   classUnderTest.addDocument("Document", new Array[Byte](0))
+ *   classUnderTest.addDocument("Document", new Array[Byte](0))
+ * }
+ * </pre>
+ *
+ * <p>
+ * ScalaTest also provides a <a href="JMockCycleFixture.html"><code>JMockCycleFixture</code></a> trait, which
+ * will pass a new <code>JMockCycle</code> into each test that needs one.
+ * </p>
+ * @author Bill Venners
+ */
 class JMockCycle {
 
   private val context = new Mockery
@@ -69,7 +201,6 @@ class JMockCycle {
   /**
    * <pre>
    * (new SuccessTestNGSuite()).runTestNG(reporter, new Tracker)
-   *
    * context.assertIsSatisfied()
    * </pre>
    *
