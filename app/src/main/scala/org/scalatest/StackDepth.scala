4b8fbd95fef0bd796ef17eca1caba6906313b2b1
bv <bv>
2009-10-06 00:38:20 +0000

    added missing scaladoc to StackDepth trait

--- app/src/main/scala/org/scalatest/StackDepth.scala
+++ app/src/main/scala/org/scalatest/StackDepth.scala
@@ -15,10 +15,32 @@
  */
 package org.scalatest
 
+/**
+ * Trait that encapsulates the information required of an exception thrown by ScalaTest's assertions
+ * and matchers, which includes a stack depth at which the failing line of test code resides.
+ *
+ * <p>
+ * This trait exists so that it can be mixed into two exception superclasses, <code>StackDepthException</code>,
+ * from which extend several exceptions that do not depend on JUnit, and <code>JUnitTestFailedError</code>, which
+ * does depend on JUnit. The latter, which requires JUnit be in the classpath, ensures failed ScalaTest assertions are
+ * reported as "failures," not "errors," by JUnit.
+ * </p>
+ */
 trait StackDepth { this: Throwable =>
 
+  /**
+   * An optional detail message for this <code>StackDepth</code> exception.
+   */
   val message: Option[String]
+ 
+  /**
+   * An optional cause, the <code>Throwable</code> that caused this <code>StackDepth</code> exception to be thrown.
+   */
   val cause: Option[Throwable]
+
+  /**
+   * The depth in the stack trace of this exception at which the line of test code that failed resides.
+   */
   val failedCodeStackDepth: Int
 
   /**
