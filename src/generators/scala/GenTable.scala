21a8b8daccba761b48d0453b6ca3cb2851d3c390
bill@artima.com <bill@artima.com>
2011-02-24 03:50:22 +0000

    more scaladoc for tableforN

--- src/generators/scala/GenTable.scala
+++ src/generators/scala/GenTable.scala
@@ -31,6 +31,87 @@ import scala.collection.generic.CanBuildFrom
 """
 
 val tableTemplate = """
+/**
+ * A table with $n$ columns.
+ *
+ * <p>
+ * This table is a sequence of <code>Tuple$n$</code> objects, where each tuple represents one row of the table.
+ * The first element of each tuple comprise the first column of the table, the second element of 
+ * each tuple comprise the second column, and so on.  This table also carries with it
+ * a <em>heading</em> tuple that gives string names to the columns of the table.
+ * </p>
+ *
+ * <p>
+ * A handy way to create a <code>TableFor$n$</code> is via an <code>apply</code> factory method in the <code>Table</code>
+ * singleton object provided by the <code>PropertyChecks</code> trait. Here's an example:
+ * </p>
+ *
+ * <pre>
+ * val examples =
+ *   Table(
+ *     ($argNames$),
+$columnsOfIndexes$
+ *   )
+ * </pre>
+ *
+ * <p>
+ * Because you supplied $n$ members in each tuple, the type you'll get back will be a <code>TableFor$n$</code>.
+ * <p>
+ *
+ * <p>
+ * The table provides an <code>apply</code> method that takes a function with a parameter list that matches
+ * the types and arity of the tuples contained in this table. The <code>apply</code> method will invoke the
+ * function with the members of each row tuple passed as arguments, in ascending order by index. (<em>I.e.</em>,
+ * the zeroth tuple is checked first, then the tuple with index 1, then index 2, and so on until all the rows
+ * have been checked (or until a failure occurs). The function represents a property of the code under test
+ * that should succeed for every row of the table. If the function returns normally, that indicates the property
+ * check succeeded for that row. If the function completes abruptly with an exception, that indicates the
+ * property check failed and the <code>apply</code> method will complete abruptly with a
+ * <code>TablePropertyCheckFailedException</code> that wraps the exception thrown by the supplied property function.
+ * </p>
+ * 
+ * <p>
+ * The usual way you'd invoke the <code>apply</code> method that checks a property is via a <code>forAll</code> method
+ * provided by trait <code>PropertyChecks</code>. The <code>forAll</code> method takes a <code>TableFor$n$</code> as its
+ * first argument, then in a curried argument list takes the property check function. It invokes <code>apply</code> on
+ * the <code>TableFor$n$</code>, passing in the property check function. Here's an example:
+ * </p>
+ *
+ * <pre>
+ * forAll (examples) { ($alphaLower$) =>
+ *   $sumOfArgs$ should equal (a * $n$)
+ * }
+ * </pre>
+ *
+ * <p>
+ * Because <code>TableFor$n$</code> is a <code>Seq[($alphaUpper$)]</code>, you can use it as a <code>Seq</code>. For example, here's how
+ * you could get a sequence of optional exceptions for each row of the table, indicating whether a property check succeeded or failed
+ * on each row of the table:
+ * </p>
+ *
+ * <pre>
+ * for (row <- examples) yield {
+ *   failureOf { row._1 should not equal (7) }
+ * }
+ * </pre>
+ *
+ * <p>
+ * Note: the <code>failureOf</code> method will execute the supplied code (a by-name parameter) and catch any exception. If no exception is thrown by the code,
+ * <code>failureOf</code> will result in <code>None</code>, indicating the "property check" succeeded. If the supplied code completes
+ * abruptly in an exception that would normally cause a test to fail, <code>failureOf</code> will result in a <code>Some</code> wrapping
+ * that exception. For example, the previous for expression would give you:
+ * </p>
+ *
+ * <pre>
+ * Vector(None, None, None, None, None, None, None, Some(org.scalatest.TestFailedException: 7 equaled 7), None, None)
+ * </pre>
+ *
+ * <p>
+ * This shows that all the property checks succeeded, except for the one at index 7.
+ * <p>
+ *
+ * @author Bill Venners 
+ */
 class TableFor$n$[$alphaUpper$](val heading: ($strings$), rows: ($alphaUpper$)*) extends IndexedSeq[($alphaUpper$)] with IndexedSeqLike[($alphaUpper$), TableFor$n$[$alphaUpper$]] {
 
   def apply(idx: Int): ($alphaUpper$) = rows(idx)
@@ -79,6 +160,12 @@ $namesAndValues$
   override def toString: String = stringPrefix + "(" + heading.toString + ", " +  rows.mkString(", ") + ")"
 }
 
+/**
+ * Companion object for class <code>TableFor$n$</code> that provides an implicit <code>canBuildFrom</code> method
+ * that enables higher order functions defined on <code>TableFor$n$</code> to return another <code>TableFor$n$</code>.
+ *
+ * @author Bill Venners 
+ */
 object TableFor$n$ {
 
   implicit def canBuildFrom[$alphaUpper$]: CanBuildFrom[TableFor$n$[$alphaUpper$], ($alphaUpper$), TableFor$n$[$alphaUpper$]] =
@@ -201,6 +288,12 @@ $columnsOfIndexes$
           for (argsIdx <- 0 until i) yield
             "\"" + "arg" + argsIdx + "\""
         val argsNamedArg = argsNamedArgSeq.mkString(",")                                  
+        val sumOfArgs = alpha.take(i).mkString(" + ")
+        val argNames = alpha.map("\"" + _ + "\"").take(i).mkString(", ")
+        val rawRows =                              
+          for (idx <- 0 to 9) yield                
+            List.fill(i)("  " + idx).mkString(" *     (", ", ", ")")
+        val columnsOfIndexes = rawRows.mkString(",\n")
         st.setAttribute("n", i)
         st.setAttribute("alphaLower", alphaLower)
         st.setAttribute("alphaUpper", alphaUpper)
@@ -208,6 +301,9 @@ $columnsOfIndexes$
         st.setAttribute("strings", strings)
         st.setAttribute("argsNamedArg", argsNamedArg)
         st.setAttribute("namesAndValues", namesAndValues)
+        st.setAttribute("sumOfArgs", sumOfArgs)
+        st.setAttribute("argNames", argNames)
+        st.setAttribute("columnsOfIndexes", columnsOfIndexes)
         bw.write(st.toString)
       }
     }
