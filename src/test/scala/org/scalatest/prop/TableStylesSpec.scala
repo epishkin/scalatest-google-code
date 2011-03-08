2d5ef5dc01a71a375ee576d2f0760e0100582010
bill@artima.com <bill@artima.com>
2011-03-08 08:06:08 +0000

    added state machine example

--- src/test/scala/org/scalatest/prop/TableStylesSpec.scala
+++ src/test/scala/org/scalatest/prop/TableStylesSpec.scala
@@ -20,35 +20,72 @@ import org.scalatest.matchers.ShouldMatchers
 
 class TableStyleSpec extends Spec with ShouldMatchers with TableDrivenPropertyChecks {
 
-  object FiboGen {
-    private var prev: Option[(Option[Int], Int)] = None
-    def next: Int =
-      prev match {
-        case None =>
-          prev = Some(None, 0)
-          0
-        case Some((None, 0)) =>
-          prev = Some((Some(0), 1))
-          1
-        case Some((Some(prev1), prev2)) =>
-          val result = prev1 + prev2
-          prev = Some((Some(prev2), result))
-          result
+  describe("A TableFor1") {
+    it("should be usable to test stateful functions") {
+
+      object FiboGen {
+        private var prev: Option[(Option[Int], Int)] = None
+        def next: Int =
+          prev match {
+            case None =>
+              prev = Some(None, 0)
+              0
+            case Some((None, 0)) =>
+              prev = Some((Some(0), 1))
+              1
+            case Some((Some(prev1), prev2)) =>
+              val result = prev1 + prev2
+              prev = Some((Some(prev2), result))
+              result
+          }
       }
+
+      val first14FiboNums =
+        Table("n", 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233)
+
+      forAll (first14FiboNums) { n => FiboGen.next should equal (n) }
+
+    }
   }
+  describe("A TableFor2") {
+    it("should be usable to test mutable objects in a state machine-like way") {
 
-  val first14FiboNums =
-    Table("n", 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233)
+      class Counter {
+        private var v = 0
+        def reset() { v = 0 }
+        def click() { v += 1 }
+        def enter(n: Int) { v = n }
+        def value = v
+      }
 
-  var prev: Option[(Int, Int)] = None
-  forAll (first14FiboNums) { n =>
-    prev match {
-      case None => 
-        prev = Some((FiboGen.next, FiboGen.next))
-      case Some((prev1, prev2)) =>
-        val next = FiboGen.next
-        next should equal (prev1 + prev2)
-        prev = Some((prev2, next))
+      abstract class Action
+      case object Start extends Action
+      case object Click extends Action
+      case class Enter(n: Int) extends Action
+
+      val stateTransitions =
+        Table(
+          ("action", "expectedValue"),
+          (Start,    0),
+          (Click,    1),
+          (Click,    2),
+          (Click,    3),
+          (Enter(5), 5),
+          (Click,    6),
+          (Enter(1), 1),
+          (Click,    2),
+          (Click,    3)
+        )
+
+      val counter = new Counter
+      forAll (stateTransitions) { (action, expectedValue) =>
+        action match {
+          case Start => counter.reset()
+          case Click => counter.click()
+          case Enter(n) => counter.enter(n)
+        }
+        counter.value should equal (expectedValue)
+      }
     }
   }
 }
