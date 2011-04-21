38245b1c4bd479e4e6ca5976fcb64036f662f852
bill@artima.com <bill@artima.com>
2011-04-21 01:15:58 +0000

    init ver

---
+++ src/test/scala/org/scalatest/EngineSpec.scala
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2001-2008 Artima, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.scalatest
+
+import events.InfoProvided
+import org.scalatest.matchers.ShouldMatchers
+
+class EngineSpec extends FlatSpec with SharedHelpers with ShouldMatchers {
+
+  "EngineSpec.getTestNamePrefix" should "return empty string for Trunk" in {
+    val engine = new Engine[() => Unit]("concurrentFunSuiteBundleMod", "FunSuite")
+    import engine._
+    getTestNamePrefix(Trunk) should be ("")
+  }
+
+  it should "return empty string for direct children of Trunk" in {
+    val engine = new Engine[() => Unit]("concurrentFunSuiteBundleMod", "FunSuite")
+    import engine._
+    val child = DescriptionBranch(Trunk, "Catherine", Some("child prefix"))
+    Trunk.subNodes ::= child
+    getTestNamePrefix(child) should be ("Catherine")
+  }
+
+  it should "return the parent's description name for DescriptionBranch grandchildren of trunk" in {
+    val engine = new Engine[() => Unit]("concurrentFunSuiteBundleMod", "FunSuite")
+    import engine._
+    val child = DescriptionBranch(Trunk, "child", Some("child prefix"))
+    Trunk.subNodes ::= child
+    val grandchild = DescriptionBranch(child, "grandchild", None)
+    child.subNodes ::= grandchild
+    getTestNamePrefix(grandchild) should be ("child grandchild")
+  }
+
+/*
+  it should "return the parent's child prefix for direct TestLeaf children of a description branch with a child prefix" in {
+    val engine = new Engine[() => Unit]("concurrentFunSuiteBundleMod", "FunSuite")
+    import engine._
+    val hasChildPrefix = DescriptionBranch(Trunk, "desc name", Some("child prefix"))
+    Trunk.subNodes ::= hasChildPrefix
+    val parentHasChildPrefix = TestLeaf(hasChildPrefix, "test name", "spec text", () => ())
+    hasChildPrefix.subNodes ::= parentHasChildPrefix
+    getTestNamePrefix(hasChildPrefix) should be ("child prefix")
+  }
+*/
+}
