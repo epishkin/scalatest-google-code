f3833599e7dc463b4e1dfca94e81e433a443e688
bill@artima.com <bill@artima.com>
2011-04-23 01:28:18 +0000

    init ver

---
+++ src/main/scala/org/scalatest/PropSpec.scala
@@ -0,0 +1,177 @@
+/*
+ * Copyright 2001-2008 Artima, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.scalatest
+
+import scala.collection.immutable.ListSet
+import java.util.ConcurrentModificationException
+import java.util.concurrent.atomic.AtomicReference
+import org.scalatest.StackDepthExceptionHelper.getStackDepth
+import org.scalatest.events._
+import Suite.anErrorThatShouldCauseAnAbort
+import Suite.checkRunTestParamsForNull
+
+trait PropSpec extends Suite { thisSuite =>
+
+  private final val engine = new Engine("concurrentPropSpecMod", "PropSpec")
+  import engine._
+
+  /**
+   * Returns an <code>Informer</code> that during test execution will forward strings (and other objects) passed to its
+   * <code>apply</code> method to the current reporter. If invoked in a constructor, it
+   * will register the passed string for forwarding later during test execution. If invoked while this
+   * <code>PropSpec</code> is being executed, such as from inside a test function, it will forward the information to
+   * the current reporter immediately. If invoked at any other time, it will
+   * throw an exception. This method can be called safely by any thread.
+   */
+  implicit protected def info: Informer = atomicInformer.get
+
+  /**
+   * Register a property-based test with the specified name, optional tags, and function value that takes no arguments.
+   * This method will register the test for later execution via an invocation of one of the <code>run</code>
+   * methods. The passed test name must not have been registered previously on
+   * this <code>PropSpec</code> instance.
+   *
+   * @param testName the name of the property
+   * @param testTags the optional list of tags for this property
+   * @param testFun the property function
+   * @throws TestRegistrationClosedException if invoked after <code>run</code> has been invoked on this suite
+   * @throws DuplicateTestNameException if a test with the same name has been registered previously
+   * @throws NotAllowedException if <code>testName</code> had been registered previously
+   * @throws NullPointerException if <code>testName</code> or any passed test tag is <code>null</code>
+   */
+  protected def property(testName: String, testTags: Tag*)(testFun: => Unit) {
+    registerTest(testName, testFun _, "propertyCannotAppearInsideAnotherProperty", "PropSpec.scala", "property", testTags: _*)
+  }
+
+  /**
+   * Register a property-based test to ignore, which has the specified name, optional tags, and function value that takes no arguments.
+   * This method will register the test for later ignoring via an invocation of one of the <code>run</code>
+   * methods. This method exists to make it easy to ignore an existing test by changing the call to <code>test</code>
+   * to <code>ignore</code> without deleting or commenting out the actual test code. The test will not be run, but a
+   * report will be sent that indicates the test was ignored. The passed test name must not have been registered previously on
+   * this <code>PropSpec</code> instance.
+   *
+   * @param testName the name of the test
+   * @param testTags the optional list of tags for this test
+   * @param testFun the test function
+   * @throws TestRegistrationClosedException if invoked after <code>run</code> has been invoked on this suite
+   * @throws DuplicateTestNameException if a test with the same name has been registered previously
+   * @throws NotAllowedException if <code>testName</code> had been registered previously
+   */
+  protected def ignore(testName: String, testTags: Tag*)(testFun: => Unit) {
+    registerIgnoredTest(testName, testFun _, "ignoreCannotAppearInsideAProperty", "PropSpec.scala", "ignore", testTags: _*)
+  }
+
+  /**
+  * An immutable <code>Set</code> of test names. If this <code>PropSpec</code> contains no tests, this method returns an empty <code>Set</code>.
+  *
+  * <p>
+  * This trait's implementation of this method will return a set that contains the names of all registered tests. The set's iterator will
+  * return those names in the order in which the tests were registered.
+  * </p>
+  */
+  override def testNames: Set[String] = {
+    // I'm returning a ListSet here so that they tests will be run in registration order
+    ListSet(atomic.get.testNamesList.toArray: _*)
+  }
+
+  /**
+   * Run a test. This trait's implementation runs the test registered with the name specified by <code>testName</code>.
+   *
+   * @param testName the name of one test to run.
+   * @param reporter the <code>Reporter</code> to which results will be reported
+   * @param stopper the <code>Stopper</code> that will be consulted to determine whether to stop execution early.
+   * @param configMap a <code>Map</code> of properties that can be used by the executing <code>Suite</code> of tests.
+   * @throws IllegalArgumentException if <code>testName</code> is defined but a test with that name does not exist on this <code>PropSpec</code>
+   * @throws NullPointerException if any of <code>testName</code>, <code>reporter</code>, <code>stopper</code>, or <code>configMap</code>
+   *     is <code>null</code>.
+   */
+  protected override def runTest(testName: String, reporter: Reporter, stopper: Stopper, configMap: Map[String, Any], tracker: Tracker) {
+
+    def invokeWithFixture(theTest: TestLeaf) {
+      val theConfigMap = configMap
+      withFixture(
+        new NoArgTest {
+          def name = testName
+          def apply() { theTest.testFun() }
+          def configMap = theConfigMap
+        }
+      )
+    }
+
+    runTestImpl(thisSuite, testName, reporter, stopper, configMap, tracker, true, invokeWithFixture)
+  }
+
+  /**
+   * A <code>Map</code> whose keys are <code>String</code> tag names to which tests in this <code>PropSpec</code> belong, and values
+   * the <code>Set</code> of test names that belong to each tag. If this <code>PropSpec</code> contains no tags, this method returns an empty <code>Map</code>.
+   *
+   * <p>
+   * This trait's implementation returns tags that were passed as strings contained in <code>Tag</code> objects passed to 
+   * methods <code>test</code> and <code>ignore</code>. 
+   * </p>
+   */
+  override def tags: Map[String, Set[String]] = atomic.get.tagsMap
+
+  /**
+   * Run zero to many of this <code>Spec</code>'s tests.
+   *
+   * @param testName an optional name of one test to run. If <code>None</code>, all relevant tests should be run.
+   *                 I.e., <code>None</code> acts like a wildcard that means run all relevant tests in this <code>Suite</code>.
+   * @param reporter the <code>Reporter</code> to which results will be reported
+   * @param stopper the <code>Stopper</code> that will be consulted to determine whether to stop execution early.
+   * @param filter a <code>Filter</code> with which to filter tests based on their tags
+   * @param configMap a <code>Map</code> of key-value pairs that can be used by the executing <code>Suite</code> of tests.
+   * @param distributor an optional <code>Distributor</code>, into which to put nested <code>Suite</code>s to be run
+   *              by another entity, such as concurrently by a pool of threads. If <code>None</code>, nested <code>Suite</code>s will be run sequentially.
+   * @param tracker a <code>Tracker</code> tracking <code>Ordinal</code>s being fired by the current thread.
+   * @throws NullPointerException if any of the passed parameters is <code>null</code>.
+   * @throws IllegalArgumentException if <code>testName</code> is defined, but no test with the specified test name
+   *     exists in this <code>Suite</code>
+   */
+  protected override def runTests(testName: Option[String], reporter: Reporter, stopper: Stopper, filter: Filter,
+      configMap: Map[String, Any], distributor: Option[Distributor], tracker: Tracker) {
+
+    runTestsImpl(thisSuite, testName, reporter, stopper, filter, configMap, distributor, tracker, info, true, runTest)
+  }
+
+  override def run(testName: Option[String], reporter: Reporter, stopper: Stopper, filter: Filter,
+      configMap: Map[String, Any], distributor: Option[Distributor], tracker: Tracker) {
+
+    runImpl(thisSuite, testName, reporter, stopper, filter, configMap, distributor, tracker, super.run)
+  }
+
+  /**
+   * Registers shared tests.
+   *
+   * <p>
+   * This method enables the following syntax for shared tests in a <code>PropSpec</code>:
+   * </p>
+   *
+   * <pre>
+   * propertiesFor(nonEmptyStack(lastValuePushed))
+   * </pre>
+   *
+   * <p>
+   * This method just provides syntax sugar intended to make the intent of the code clearer.
+   * Because the parameter passed to it is
+   * type <code>Unit</code>, the expression will be evaluated before being passed, which
+   * is sufficient to register the shared tests. For examples of shared tests, see the
+   * <a href="#SharedTests">Shared tests section</a> in the main documentation for this trait.
+   * </p>
+   */
+  protected def propertiesFor(unit: Unit) {}
+}
