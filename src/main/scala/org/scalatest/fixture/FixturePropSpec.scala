5fffea79d614c9b3035eda17fd1508d4db128cc1
bill@artima.com <bill@artima.com>
2011-04-23 01:38:19 +0000

    init ver

---
+++ src/main/scala/org/scalatest/fixture/FixturePropSpec.scala
@@ -0,0 +1,183 @@
+/*
+ * Copyright 2001-2009 Artima, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.scalatest.fixture
+
+import org.scalatest._
+import scala.collection.immutable.ListSet
+import java.util.ConcurrentModificationException
+import java.util.concurrent.atomic.AtomicReference
+import org.scalatest.StackDepthExceptionHelper.getStackDepth
+import org.scalatest.events._
+import Suite.anErrorThatShouldCauseAnAbort
+import Suite.checkRunTestParamsForNull
+
+trait FixturePropSpec extends FixtureSuite { thisSuite =>
+
+  private final val engine = new FixtureEngine[FixtureParam]("concurrentFixturePropSpecMod", "FixturePropSpec")
+  import engine._
+
+  /**
+   * Returns an <code>Informer</code> that during test execution will forward strings (and other objects) passed to its
+   * <code>apply</code> method to the current reporter. If invoked in a constructor, it
+   * will register the passed string for forwarding later during test execution. If invoked while this
+   * <code>FixturePropSpec</code> is being executed, such as from inside a test function, it will forward the information to
+   * the current reporter immediately. If invoked at any other time, it will
+   * throw an exception. This method can be called safely by any thread.
+   */
+  implicit protected def info: Informer = atomicInformer.get
+
+  /**
+   * Register a property-based test with the specified name, optional tags, and function value that takes no arguments.
+   * This method will register the test for later execution via an invocation of one of the <code>run</code>
+   * methods. The passed test name must not have been registered previously on
+   * this <code>PropSpec</code> instance.
+   *
+   * @param testName the name of the test
+   * @param testTags the optional list of tags for this test
+   * @param testFun the test function
+   * @throws TestRegistrationClosedException if invoked after <code>run</code> has been invoked on this suite
+   * @throws DuplicateTestNameException if a test with the same name has been registered previously
+   * @throws NotAllowedException if <code>testName</code> had been registered previously
+   * @throws NullPointerException if <code>testName</code> or any passed test tag is <code>null</code>
+   */
+  protected def property(testName: String, testTags: Tag*)(testFun: FixtureParam => Any) {
+    registerTest(testName, testFun, "testCannotAppearInsideAnotherTest", "FixturePropSpec.scala", "test", testTags: _*)
+  }
+
+  /**
+   * Register a property-based test to ignore, which has the specified name, optional tags, and function value that takes no arguments.
+   * This method will register the test for later ignoring via an invocation of one of the <code>run</code>
+   * methods. This method exists to make it easy to ignore an existing test by changing the call to <code>test</code>
+   * to <code>ignore</code> without deleting or commenting out the actual test code. The test will not be run, but a
+   * report will be sent that indicates the test was ignored. The passed test name must not have been registered previously on
+   * this <code>PropSpec</code> instance.
+   *
+   * @param testName the name of the test
+   * @param testTags the optional list of tags for this test
+   * @param testFun the test function
+   * @throws TestRegistrationClosedException if invoked after <code>run</code> has been invoked on this suite
+   * @throws DuplicateTestNameException if a test with the same name has been registered previously
+   * @throws NotAllowedException if <code>testName</code> had been registered previously
+   */
+  protected def ignore(testName: String, testTags: Tag*)(testFun: FixtureParam => Any) {
+    registerIgnoredTest(testName, testFun, "ignoreCannotAppearInsideATest", "FixturePropSpec.scala", "ignore", testTags: _*)
+  }
+
+  /**
+  * An immutable <code>Set</code> of test names. If this <code>FixturePropSpec</code> contains no tests, this method returns an empty <code>Set</code>.
+  *
+  * <p>
+  * This trait's implementation of this method will return a set that contains the names of all registered tests. The set's iterator will
+  * return those names in the order in which the tests were registered.
+  * </p>
+  */
+  override def testNames: Set[String] = {
+    // I'm returning a ListSet here so that they tests will be run in registration order
+    ListSet(atomic.get.testNamesList.toArray: _*)
+  }
+
+  /**
+   * Run a test. This trait's implementation runs the test registered with the name specified by <code>testName</code>.
+   *
+   * @param testName the name of one test to run.
+   * @param reporter the <code>Reporter</code> to which results will be reported
+   * @param stopper the <code>Stopper</code> that will be consulted to determine whether to stop execution early.
+   * @param configMap a <code>Map</code> of properties that can be used by the executing <code>Suite</code> of tests.
+   * @throws IllegalArgumentException if <code>testName</code> is defined but a test with that name does not exist on this <code>FixturePropSpec</code>
+   * @throws NullPointerException if any of <code>testName</code>, <code>reporter</code>, <code>stopper</code>, or <code>configMap</code>
+   *     is <code>null</code>.
+   */
+  protected override def runTest(testName: String, reporter: Reporter, stopper: Stopper, configMap: Map[String, Any], tracker: Tracker) {
+
+    def invokeWithFixture(theTest: TestLeaf) {
+      theTest.testFun match {
+        case wrapper: NoArgTestWrapper[_] =>
+          withFixture(new FixturelessTestFunAndConfigMap(testName, wrapper.test, configMap))
+        case fun => withFixture(new TestFunAndConfigMap(testName, fun, configMap))
+      }
+    }
+
+    runTestImpl(thisSuite, testName, reporter, stopper, configMap, tracker, true, invokeWithFixture)
+  }
+
+  /**
+   * A <code>Map</code> whose keys are <code>String</code> tag names to which tests in this <code>FixturePropSpec</code> belong, and values
+   * the <code>Set</code> of test names that belong to each tag. If this <code>FixturePropSpec</code> contains no tags, this method returns an empty
+   * <code>Map</code>.
+   *
+   * <p>
+   * This trait's implementation returns tags that were passed as strings contained in <code>Tag</code> objects passed to
+   * methods <code>test</code> and <code>ignore</code>.
+   * </p>
+   */
+  override def tags: Map[String, Set[String]] = atomic.get.tagsMap
+
+  protected override def runTests(testName: Option[String], reporter: Reporter, stopper: Stopper, filter: Filter,
+      configMap: Map[String, Any], distributor: Option[Distributor], tracker: Tracker) {
+
+    runTestsImpl(thisSuite, testName, reporter, stopper, filter, configMap, distributor, tracker, info, true, runTest)
+  }
+
+  override def run(testName: Option[String], reporter: Reporter, stopper: Stopper, filter: Filter,
+      configMap: Map[String, Any], distributor: Option[Distributor], tracker: Tracker) {
+
+    runImpl(thisSuite, testName, reporter, stopper, filter, configMap, distributor, tracker, super.run)
+  }
+
+  /**
+   * Registers shared tests.
+   *
+   * <p>
+   * This method enables the following syntax for shared tests in a <code>FixturePropSpec</code>:
+   * </p>
+   *
+   * <pre>
+   * testsFor(nonEmptyStack(lastValuePushed))
+   * </pre>
+   *
+   * <p>
+   * This method just provides syntax sugar intended to make the intent of the code clearer.
+   * Because the parameter passed to it is
+   * type <code>Unit</code>, the expression will be evaluated before being passed, which
+   * is sufficient to register the shared tests. For examples of shared tests, see the
+   * <a href="../PropSpec.html#SharedTests">Shared tests section</a> in the main documentation for
+   * trait <code>PropSpec</code>.
+   * </p>
+   */
+  protected def testsFor(unit: Unit) {}
+
+  /**
+   * Implicitly converts a function that takes no parameters and results in <code>PendingNothing</code> to
+   * a function from <code>Fixture</code> to <code>Any</code>, to enable pending tests to registered as by-name parameters
+   * by methods that require a test function that takes a <code>Fixture</code>.
+   *
+   * <p>
+   * This method makes it possible to write pending tests as simply <code>(pending)</code>, without needing
+   * to write <code>(fixture => pending)</code>.
+   * </p>
+   */
+  protected implicit def convertPendingToFixtureFunction(f: => PendingNothing): (FixtureParam => Any) = {
+    fixture => f
+  }
+
+  /**
+   * Implicitly converts a function that takes no parameters and results in <code>Any</code> to
+   * a function from <code>Fixture</code> to <code>Any</code>, to enable no-arg tests to registered
+   * by methods that require a test function that takes a <code>Fixture</code>.
+   */
+  protected implicit def convertNoArgToFixtureFunction(fun: () => Any): (FixtureParam => Any) =
+    new NoArgTestWrapper(fun)
+}
