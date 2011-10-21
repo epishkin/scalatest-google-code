1bd4efeaa09081dd878fc38e44ecbbc057d4c43d
bill%artima.com@gtempaccount.com <bill%artima.com@gtempaccount.com>
2011-10-21 01:29:48 +0000

    sleep a second so don't overwrite files; Need to do millis

--- src/us/runem.scala
+++ src/us/runem.scala
@@ -2,9 +2,11 @@
 import org.scalatest.tools.Runner
 
 object runem extends App {
-  for (i <- 1 to 200)
+  for (i <- 1 to 200) {
     Runner.run(
       Array("-p", ".", "-o", "-F", "dashboard", "-s", "UnitedStates")
     )
+    Thread.sleep(1000)
+  }
 }
 
