09d08093c9d13e2ae9d66f2d0a0969d4b04ceb23
billvenners@gmail.com <billvenners@gmail.com>
2011-11-23 03:12:35 +0000

    Some documentation on AppendedClues.

--- src/main/scala/org/scalatest/AppendedClues.scala
+++ src/main/scala/org/scalatest/AppendedClues.scala
@@ -15,9 +15,128 @@
  */
 package org.scalatest
 
-trait AppendedClues {
+/**
+ * Trait providing an implicit conversion that allows clues to be place after a block of code.
+ *
+ * <p>
+ * You can use the <code>withClue</code> construct provided by <code>Assertions</code>, which is
+ * extended by every style trait in ScalaTest, to add extra information to reports of failed or canceled tests.
+ * The <code>withClue</code> from <code>Assertions</code> places the "clue string" at the front, both
+ * in the code and in the resulting message:
+ *
+ * <pre class="stHighlight">
+ * withClue("This is a prepended clue;") {
+ *   1 + 1 should equal (3)
+ * }
+ * </pre>
+ *
+ * <p>
+ * The above expression will yield the failure message:
+ * </p>
+ *
+ * <p>
+ * <code>This is a prepended clue; 2 did not equal 3</code>
+ * </p>
+ *
+ * <p>
+ * If you mix in this trait, or import its members via its companion object, you can alternatively place
+ * the clue string at the end, like this:
+ * </p>
+ *
+ * <pre class="stHighlight">
+ * { 1 + 1 should equal (3) } withClue "now the clue comes after"
+ * </pre>
+ *
+ * <p>
+ * The above expression will yield the failure message:
+ * </p>
+ *
+ * <p>
+ * <code>2 did not equal 3 now the clue comes after</code>
+ * </p>
+ *
+ * <p>
+ * If no space is already present, either at the beginning of the clue string or at the end
+ * of the current message, a space will be placed between the two, unless the clue string
+ * starts with one of the punctuation characters: comma (<code>,</code>), period (<code>.</code>),
+ * or semicolon (<code>;</code>). For example, the failure message in the above example
+ * includes an extra space inserted between <em>3</em> and <em>now</em>.
+ * </p>
+ *
+ * <p>
+ * By contrast this code, which has a clue string starting with comma:
+ * </p>
+ *
+ * <pre class="stHighlight">
+ * { 1 + 1 should equal (3) } withClue ", now the clue comes after"
+ * </pre>
+ *
+ * <p>
+ * Will yield a failure message with no extra inserted space:
+ * </p>
+ *
+ * <p>
+ * <code>2 did not equal 3, now the clue comes after</code>
+ * </p>
+ *
+ * <p>
+ * The <code>withClue</code> method will only append the clue string to the detail
+ * message of exception types that mix in the <code>ModifiableMessage</code> trait.
+ * See the documentation for <a href="ModifiableMessage.html"><code>ModifiableMessage</code></a> for more
+ * information.
+ * </p>
+ *
+ * <p>
+ * Note: the reason this functionality is not provided by <code>Assertions</code> directly, like the
+ * prepended <code>withClue</code> construct, is because appended clues require an implicit conversion.
+ * ScalaTest only gives you one implicit conversion by default in any test class to minimize the
+ * potential for conflicts with other implicit conversions you may be using. All other implicit conversions,
+ * including the one provided by this trait, you must explicitly invite into your code through inheritance
+ * or an import.
+ * </p>
+ *
+ * @author Bill Venners
+ */
+trait AppendedClues { // TODO, can't put a comma. Write tests to not put the space if a ,, ., or ; is the first char
 
+// TODO: Also, make sure withClue returns whatever is inside there. I think I should do this for the prepended
+// withClue also.
+  /**
+   * Class that provides a <code>withClue</code> method that appends clue strings to any
+   * <a href="ModifiableMessage.html"><code>ModifiableMessage</code></a> exception
+   * thrown by the passed by-name parameter.
+   *
+   * @author Bill Venners
+   */
   class Clueful(fun: => Any) {
+
+    /**
+     * Executes the block of code passed as the constructor parameter to this <code>Clueful</code>, and, if it
+     * completes abruptly with a <code>ModifiableMessage</code> exception,
+     * appends the "clue" string passed to this method to the end of the detail message
+     * of that thrown exception, then rethrows it. If clue does not begin in a white space
+     * character or one of the punctuation characters: comma (<code>,</code>),
+     * period (<code>.</code>), or semicolon (<code>;</code>), one space will be added
+     * between it and the existing detail message (unless the detail message is
+     * not defined).
+     *
+     * <p>
+     * This method allows you to add more information about what went wrong that will be
+     * reported when a test fails or cancels. For example, this code:
+     * </p>
+     *
+     * <pre class="stHighlight">
+     * { 1 + 1 should equal (3) } withClue ", not even for very large values of 1"
+     * </pre>
+     *
+     * <p>
+     * Would yield a <code>TestFailed</code> exception whose message would be:
+     * </p>
+     *
+     * <pre>
+     * 2 did not equal 3, not even for very large values of 1
+     * </pre>
+     */
     def withClue(clue: Any) {
       def append(currentMessage: Option[String]) =
         currentMessage match {
