a8ef7f3def170b395dd314ef882aabf2b6ed6495
billvenners@gmail.com <billvenners@gmail.com>
2011-12-27 20:49:55 +0000

    enhanced deprecation scaladoc

--- src/main/scala/org/scalatest/fixture/FixtureSpec.scala
+++ src/main/scala/org/scalatest/fixture/FixtureSpec.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2001-2009 Artima, Inc.
+ * Copyright 2001-2011 Artima, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,6 +21,21 @@ package org.scalatest.fixture
  * release of ScalaTest. Please change any uses of <code>org.scalatest.fixture.FixtureSpec</code>
  * to a corresponding use of <a href="FunSpec.html"><code>org.scalatest.fixture.FunSpec</code></a>.</strong>
  * 
+ * <p>
+ * <strong> This is just
+ * a rename, so the only thing you need to do is change the name. However, the recommended way to
+ * write it is to import <code>org.scalatest.fixture</code> and then write <code>fixture.FunSpec</code> when
+ * you use it, to differentiate it more clearly from <code>org.scalatest.FunSpec</code>. For example:
+ * </strong>
+ * </p>
+ *
+ * <pre>
+ * import org.scalatest.fixture
+ *
+ * class ExampleSpec extends fixture.FunSpec {
+ *   // ...
+ * }
+ * </pre>
  */
 @deprecated("Please use org.scalatest.fixture.FunSpec instead.")
 trait FixtureSpec extends FunSpec {
