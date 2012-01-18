4c9fa1b22154e773d60b23a4bd0c34d5810bad59
billvenners@gmail.com <billvenners@gmail.com>
2012-01-18 05:26:28 +0000

    Tweaked javadoc for wrapwith.

--- src/main/java/org/scalatest/WrapWith.java
+++ src/main/java/org/scalatest/WrapWith.java
@@ -21,10 +21,13 @@ import java.lang.annotation.*;
  * Annotation to associate a <em>wrapper suite</em> with a non-<code>Suite</code> class, so it can be run via ScalaTest.
  *
  * <p>
+ * A class will be considered annotated with <code>WrapWith</code> if it is annotated directly or one of its superclasses (but
+ * not supertraits) are annotated with <code>WrapWith</code>.
  * The wrapper suite must have a public, one-arg constructor that takes a <code>Class</code> instance whose type parameter
  * is compatible with the <em>class to wrap</em>: <em>i.e.</em>, the class being annotated with <code>WrapWith</code>.
  * ScalaTest will load the class to wrap and construct a new instance of the wrapper suite, passing in the <code>Class</code>
- * instance for the class to wrap. Here's an example:
+ * instance for the class to wrap.
+ * Here's an example:
  * </p>
  *
  * <pre class="stHighlight">
